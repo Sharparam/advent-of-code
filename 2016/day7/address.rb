8ba52fffc7e0f95a5325975fd1b8cfd530a9ea40
Adam Hellberg <sharparam@sharparam.com>
2016-12-08 00:39:49 +0100

    Shorten code for 2016:7

--- 2016/day7/address.rb
+++ 2016/day7/address.rb
@@ -4,13 +4,11 @@
 class Address
   ABBA_PATTERN = /([a-z0-9])(?!\1)([a-z0-9])\2\1/
 
-  ABA_PATTERN = /(?=(([a-z0-9])(?!\2)[a-z0-9]\2))/
-
   def initialize(input)
     @raw = input
 
-    init_supernets!
-    init_hypernets!
+    @supernets = @raw.scan(/[^\]][a-z0-9]+[^\[]/).select { |p| p[0] != '[' }
+    @hypernets = @raw.scan(/(?<=[\[])[a-z0-9]+(?=[\]])/)
   end
 
   def tls?
@@ -19,20 +17,10 @@ class Address
 
   def ssl?
     @supernets.any? do |snet|
-      snet.scan(ABA_PATTERN).any? do |data|
+      snet.scan(/(?=(([a-z0-9])(?!\2)[a-z0-9]\2))/).any? do |data|
         inverse = [data.first[1], data.first[0], data.first[1]].join
         @hypernets.any? { |hnet| hnet.include? inverse }
       end
     end
-  end
-
-  private
-
-  def init_supernets!
-    @supernets = @raw.scan(/[^\]][a-z0-9]+[^\[]/).select { |p| p[0] != '[' }
-  end
-
-  def init_hypernets!
-    @hypernets = @raw.scan(/(?<=[\[])[a-z0-9]+(?=[\]])/)
   end
 end
