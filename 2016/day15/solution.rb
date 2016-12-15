4625e880ee1ec5682c361d1b985b378ae0ffd14a
Adam Hellberg <sharparam@sharparam.com>
2016-12-15 11:15:18 +0100

    Add solution for 2016:15

---
+++ 2016/day15/solution.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+# frozen_string_literal: true
+
+require 'ostruct'
+
+discs = STDIN.readlines.map do |line|
+  size, start = line.strip.match(/(\d+) positions.+?position (\d+)/).to_a[1..2].map(&:to_i)
+  OpenStruct.new size: size, position: start
+end
+
+def test?(discs, start)
+  discs.all? { |disc| (disc.position + (start += 1)) % disc.size == 0 }
+end
+
+start = -1
+
+loop until test? discs, (start += 1)
+
+puts "(1) #{start}"
+
+start = -1
+
+discs.push OpenStruct.new size: 11, position: 0
+
+loop until test? discs, (start += 1)
+
+puts "(2) #{start}"
