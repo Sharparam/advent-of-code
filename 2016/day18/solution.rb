b465098b8f1a951f16ca0c5fd803a65c86496d3b
Adam Hellberg <sharparam@sharparam.com>
2016-12-18 15:52:39 +0100

    Add solution for 2016:18

---
+++ 2016/day18/solution.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+# frozen_string_literal: true
+
+HEIGHT = (ARGV.first == 't') ? 400000 : 40
+
+class Row
+  def initialize(tiles)
+    @tiles = tiles
+  end
+
+  def width
+    @tiles.size
+  end
+
+  def trap?(index)
+    return false if index < 0 || index >= @tiles.size
+    @tiles[index]
+  end
+
+  def left?(index)
+    trap? index - 1
+  end
+
+  def right?(index)
+    trap? index + 1
+  end
+
+  def center?(index)
+    trap? index
+  end
+
+  def count_safe
+    @tiles.reduce(0) { |a, e| a += (e ? 0 : 1) }
+  end
+
+  def to_s
+    @tiles.map { |t| t ? '^' : '.' }.join
+  end
+end
+
+class Room
+  def initialize(row)
+    @rows = [Row.new(row.map { |t| t == '^' })]
+  end
+
+  def row_count
+    @rows.size
+  end
+
+  def make_row
+    previous = @rows.last
+    data = previous.width.times.map do |index|
+      (previous.left?(index) && previous.center?(index) && !previous.right?(index)) ||
+      (!previous.left?(index) && previous.center?(index) && previous.right?(index)) ||
+      (previous.left?(index) && !previous.center?(index) && !previous.right?(index)) ||
+      (!previous.left?(index) && !previous.center?(index) && previous.right?(index))
+    end
+    @rows << Row.new(data)
+  end
+
+  def count_safe
+    @rows.reduce(0) { |a, e| a += e.count_safe }
+  end
+
+  def to_s
+    @rows.map(&:to_s).join "\n"
+  end
+end
+
+input = STDIN.readline.strip.split('')
+
+room = Room.new input
+
+(HEIGHT - 1).times { room.make_row }
+
+puts room.count_safe
