aa2ded5e4fa134fcb8a9c819adfa94f20841cfd6
Adam Hellberg <sharparam@sharparam.com>
2016-12-20 19:07:37 +0100

    Add solution for 2016:19

---
+++ 2016/day19/solution.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+# frozen_string_literal: true
+
+class Elf
+  attr_reader :id
+
+  attr_accessor :previous, :next
+
+  def initialize(id)
+    @id = id
+  end
+
+  def delete!
+    @next.previous = @previous
+    @previous.next = @next
+  end
+end
+
+def make_elves(count)
+  elves = count.times.map { |i| Elf.new(i + 1) }
+  elves.each.with_index do |elf, i|
+    elf.previous = elves[(i - 1) % count]
+    elf.next = elves[(i + 1) % count]
+  end
+  elves
+end
+
+elves = make_elves STDIN.readline.strip.to_i
+
+SIZE = elves.size
+
+start = elves.first
+
+while start.next != start
+  start.next.delete!
+  start = start.next
+end
+
+puts "(1) #{start.id}"
+
+elves = make_elves SIZE
+
+start = elves.first
+mid = elves[SIZE / 2]
+
+SIZE.times do |i|
+  mid.delete!
+  mid = mid.next
+  mid = mid.next if (SIZE - i) % 2 == 1
+  start = start.next
+end
+
+puts "(2) #{start.id}"
