06252e21f6e6331e925d269e0d178700e2f054fe
Adam Hellberg <sharparam@sharparam.com>
2017-12-05 00:03:44 +0100

    Make 2017 day 4 shorter

--- 2017/day4/solution.rb
+++ 2017/day4/solution.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 
-input = STDIN.readlines.map { |l| l.strip.split }
-p input.count { |e| e.uniq == e }
-p input.map { |l| l.map { |w| w.chars.sort }}.count { |e| e.uniq == e }
+d = $<.readlines.map(&:split)
+p d.count { |e| e.uniq == e }
+p d.map { |l| l.map { |w| w.chars.sort }}.count { |e| e.uniq == e }
