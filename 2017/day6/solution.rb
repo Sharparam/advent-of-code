6e1ec71fb6953a7f378ba265482fa09f1d2e49ad
Adam Hellberg <sharparam@sharparam.com>
2017-12-07 22:18:57 +0100

    Add Ruby solution for day 6

---
+++ 2017/day6/solution.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+require 'set'
+
+memory = $<.readline.split.map(&:to_i)
+
+configs = Set.new
+redists = 0
+
+def redistribute(array)
+    blocks = array.max
+    index = array.index blocks
+    array[index] = 0
+    while blocks > 0
+        index = (index + 1) % array.size
+        array[index] += 1
+        blocks -= 1
+    end
+end
+
+while !configs.include? memory
+    configs.add memory.clone
+    redistribute memory
+    redists += 1
+end
+
+p redists
+
+state = memory.clone
+
+steps = 1
+redistribute memory
+while memory != state
+    redistribute memory
+    steps += 1
+end
+
+p steps
