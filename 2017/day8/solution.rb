92968a4ff1460d7c2812fb3eba085986f0816a19
Adam Hellberg <sharparam@sharparam.com>
2017-12-08 19:04:08 +0100

    Who needs accessors
    When you have instance_variable_get?

--- 2017/day8/solution.rb
+++ 2017/day8/solution.rb
@@ -1,19 +1,14 @@
 #!/usr/bin/env ruby
 
 class CPU
-    attr_reader :high, :regs
-
-    def initialize; @regs = Hash.new(0) end
-    def inc(value) value end
-    def dec(value) -value end
+    def initialize; @r = Hash.new(0) end
+    def inc(v) v end
+    def dec(v) -v end
     def run(text) eval text end
-
-    def method_missing(sym, *args)
-        (@regs[sym] += args[0].to_i).tap { |v| @high = v if @high.nil? || @high < v }
-    end
+    def method_missing(s, *a) (@r[s] += a[0].to_i).tap { |v| @h = v if @h.nil? || @h < v } end
 end
 
 cpu = CPU.new
 cpu.run $<.read
-puts cpu.regs.values.max
-puts cpu.high
+puts cpu.instance_variable_get('@r').values.max
+puts cpu.instance_variable_get('@h')
