06cb0faf4e1b313dd7e75f2e8ac781675ba24807
Adam Hellberg <sharparam@sharparam.com>
2017-12-08 18:58:33 +0100

    Compact further

--- 2017/day8/solution.rb
+++ 2017/day8/solution.rb
@@ -1,24 +1,19 @@
 #!/usr/bin/env ruby
 
 class CPU
-    attr_reader :highest, :regs
+    attr_reader :high, :regs
 
-    def initialize
-        @regs = Hash.new(0)
-    end
-
+    def initialize; @regs = Hash.new(0) end
     def inc(value) value end
     def dec(value) -value end
     def run(text) eval text end
 
-    def method_missing(symbol, *args)
-        (@regs[symbol] += args.first.to_i).tap do |val|
-            @highest = val if @highest.nil? || @highest < val
-        end
+    def method_missing(sym, *args)
+        (@regs[sym] += args[0].to_i).tap { |v| @high = v if @high.nil? || @high < v }
     end
 end
 
 cpu = CPU.new
 cpu.run $<.read
 puts cpu.regs.values.max
-puts cpu.highest
+puts cpu.high
