ac072ce6e52b122ea9c003a19bec081ba703d2e2
Adam Hellberg <sharparam@sharparam.com>
2018-12-01 18:23:18 +0100

    Improve Ruby solution for 2018 Day 1

--- 2018/day1/solution.rb
+++ 2018/day1/solution.rb
@@ -4,21 +4,13 @@ freqs = File.readlines('input.txt').map(&:to_i)
 
 puts "Part 1: #{freqs.reduce(0) { |a, e| a + e }}"
 
-occurrences = Hash.new(0).tap { |h| h[0] = 1 }
-found = nil
+multiples = Set.new
 freq = 0
-ind = 0
 
-while !found do
-    freq += freqs[ind]
-    occurrences[freq] += 1
-
-    if occurrences[freq] > 1
-        found = freq
-    end
-
-    ind += 1
-    ind = 0 if ind >= freqs.size
+freqs.cycle do |current|
+    freq += current
+    break if multiples.include? freq
+    multiples.add freq
 end
 
-puts "Part 2: #{found}"
+puts "Part 2: #{freq}"
