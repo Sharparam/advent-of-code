00eca511d78fc772133b362d58e19bbe42bd81b8
Adam Hellberg <sharparam@sharparam.com>
2018-12-01 15:53:22 +0100

    Add files for 2017 Day 3 and 11

---
+++ 2017/day3/solution.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require 'matrix'
+
+input = STDIN.readline.strip.to_i
+
+steps = (Math.sqrt(input).ceil / 2).floor
+offset = (input - (2 * steps - 1)**2) % (2 * steps)
+steps = steps + (offset - steps).abs
+
+puts "Part 1: #{steps}"
+
+# Part 2 from OEIS: https://oeis.org/A141481
