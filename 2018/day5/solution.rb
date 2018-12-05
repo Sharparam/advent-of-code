6bb9cefcfe70d8e7ec23d6086f9a2d773845c218
Adam Hellberg <adam.hellberg@ninetech.com>
2018-12-05 11:33:33 +0100

    Add naïve Ruby solution for 2018 D05
    Note that this solution runs extremely slow.

---
+++ 2018/day5/solution.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+
+require 'set'
+require 'pry'
+
+input = File.read('test.txt').strip
+
+part1 = input.split('')
+
+def react(str)
+  arr = str.split('')
+  replaced = true
+
+  while replaced
+    replaced = false
+
+    bad_indices = Set.new
+
+    arr.each.with_index do |e, i|
+      n = arr[i..i+1]
+      if n.map(&:upcase).uniq.size == 1 && n.uniq.size == 2
+        #arr.slice!(i..i+1)
+        bad_indices.add i
+        bad_indices.add i + 1
+        replaced = true
+      end
+    end
+
+    arr.reject!.with_index { |_, i| bad_indices.include? i }
+  end
+
+  arr
+end
+
+part1 = react input
+
+puts "Part 1: #{part1.size}"
+
+exit
+
+letters = input.upcase.split('').uniq
+
+puts letters
+
+smallest = input.size
+
+#binding.pry
+
+letters.each.with_index do |letter, index|
+  puts "Testing with letter #{letter} (#{index + 1} / #{letters.size})"
+  new_input = input.gsub /#{letter}/i, ''
+  reacted = react new_input
+  smallest = reacted.size if reacted.size < smallest
+end
+
+puts "Part 2: #{smallest}"
