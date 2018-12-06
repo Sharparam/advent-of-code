cc8a2b329b5fc711232c60a72055a224da4a90f2
Adam Hellberg <sharparam@sharparam.com>
2018-12-06 23:41:20 +0100

    Update Ruby for 2018 D06

--- 2018/day6/solution.rb
+++ 2018/day6/solution.rb
@@ -1,39 +1,45 @@
 #!/usr/bin/env ruby
 
-PART2_THRESHOLD = 10000
-
 coords = File.read('input.txt').scan(/(\d+), (\d+)/).map { |ns| ns.map(&:to_i) }
-width = coords.map(&:first).max + 1
-height = coords.map(&:last).max + 1
-areas = Hash.new { |h, k| h[k] = [] }
 
-def dist(a, b); (a[0] - b[0]).abs + (a[1] - b[1]).abs; end
-def find(coords, i)
-  dists = Hash[coords.map { |c| [c, dist(i, c)] }]
-  min = dists.values.min
-  filtered = dists.select { |k, v| v == min }
-  filtered.size > 1 ? nil : filtered.keys.first
-end
+xmin, xmax = coords.map(&:first).minmax
+ymin, ymax = coords.map(&:last).minmax
 
-part2_area = 0
+counts = [0] * coords.size
+invalids = [false] * coords.size
 
-(0...width).each do |x|
-  (0...height).each do |y|
-    point = [x, y]
-    closest = find(coords, point)
-    part2_dist = coords.map { |c| dist(c, point) }.sum
-    part2_area += 1 if part2_dist < PART2_THRESHOLD
-    next unless closest
-    areas[closest] << point
+part2 = 0
+
+(xmin..xmax).each do |x|
+  x_edge = x == xmin || x == xmax
+  x_dists = coords.map { |cx, _| (x - cx).abs }
+  (ymin..ymax).each do |y|
+    y_edge = y == ymin || y == ymax
+    best_i = nil
+    best_dist = 1.0 / 0.0
+    dist_total = 0
+    coords.each_with_index do |(_, cy), i|
+      dist = x_dists[i] + (y - cy).abs
+      if dist < best_dist
+        best_dist = dist
+        best_i = i
+      elsif dist == best_dist
+        best_i = nil
+      end
+      dist_total += dist
+    end
+
+    part2 += 1 if dist_total < 10000
+
+    next unless best_i
+
+    if x_edge || y_edge
+      invalids[best_i] = true
+    else
+      counts[best_i] += 1
+    end
   end
 end
 
-largest = areas.reject do |k, v|
-  v.any? { |e| e.first == 0 || e.first == width - 1 || e.last == 0 || e.last == height - 1 }
-end.max { |a, b| a.last.size <=> b.last.size }.last.size
-
-puts "Part 1: #{largest}"
-puts "Part 2: #{part2_area}"
-
-# require 'pry'
-# binding.pry
+puts "Part 1: #{counts.zip(invalids).reject(&:last).map(&:first).max}"
+puts "Part 2: #{part2}"
