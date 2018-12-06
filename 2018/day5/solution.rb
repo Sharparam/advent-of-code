623f83b2974592870b72eed3c0a5e5e4a42626db
Adam Hellberg <sharparam@sharparam.com>
2018-12-06 19:09:25 +0100

    Update Ruby for 2018 D05

--- 2018/day5/solution.rb
+++ 2018/day5/solution.rb
@@ -1,50 +1,26 @@
 #!/usr/bin/env ruby
 
-require 'set'
+input = File.read('input.txt').chomp
+ords = input.chars.map(&:ord)
 
-input = File.read('input.txt').strip
+def react(os)
+  res = [0]
 
-part1 = input.split('')
-
-def react(str)
-  arr = str.split('')
-  replaced = true
-
-  while replaced
-    replaced = false
-
-    bad_indices = Set.new
-
-    arr.each.with_index do |e, i|
-      n = arr[i..i+1]
-      if n.map(&:upcase).uniq.size == 1 && n.uniq.size == 2
-        bad_indices.add i
-        bad_indices.add i + 1
-        replaced = true
-      end
+  os.each do |o|
+    if res[-1] ^ o == 32
+      res.pop
+    else
+      res << o
     end
-
-    arr.reject!.with_index { |_, i| bad_indices.include? i }
   end
 
-  arr
+  res[1..-1]
 end
 
-part1 = react input
-
+part1 = react ords
 puts "Part 1: #{part1.size}"
 
-letters = input.upcase.split('').uniq
+exit
 
-puts letters
-
-smallest = input.size
-
-letters.each.with_index do |letter, index|
-  puts "Testing with letter #{letter} (#{index + 1} / #{letters.size})"
-  new_input = input.gsub /#{letter}/i, ''
-  reacted = react new_input
-  smallest = reacted.size if reacted.size < smallest
-end
-
-puts "Part 2: #{smallest}"
+part2 = (?a.ord..?z.ord).map { |l| react(part1.reject { |e| e == l || e == l + 32 }).size }.min
+puts "Part 2: #{part2}"
