d979c66d611b26cb716547441fe77b7e474db2fa
Adam Hellberg <sharparam@sharparam.com>
2018-12-08 22:40:15 +0100

    Add a script

---
+++ 2018/day8/excel.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+# For @Tim#5138 on the AoC discord
+
+LETTERS = ('A'..'Z').to_a.freeze
+
+def to_excel n
+  return LETTERS[n] if n < LETTERS.size
+  q, r = n.divmod LETTERS.size
+  to_excel(q - 1) + LETTERS[r]
+end
+
+def pe n; printf("%4d == %4s\n", n, to_excel(n)); end
+
+pe 0
+pe 1
+pe 20
+pe 25
+pe 26
+pe 27
+
+#  AA   AA    Z   A
+pe 26 * 26 + 25 + 1
