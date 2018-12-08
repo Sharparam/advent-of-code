b8cf65eb73fa31fbb201fb7364118f7104e482ed
Adam Hellberg <sharparam@sharparam.com>
2018-12-08 16:33:02 +0100

    Shave off one byte from Ruby 2018 D08P1

--- 2018/day8/part1.rb
+++ 2018/day8/part1.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 
-def f d, s; a = d.shift 2; s + a[0].times.map { f d, s }.sum + d.shift(a[1]).sum end
+def f d, s; (c, m) = d.shift 2; s + c.times.map { f d, s }.sum + d.shift(m).sum end
 puts f File.read('input.txt').split.map(&:to_i), 0
