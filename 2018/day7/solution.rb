8f483cc6b8bc8eaff3a51d97e0264873ae706c22
Adam Hellberg <sharparam@sharparam.com>
2018-12-08 14:35:14 +0100

    Complete Ruby solution for 2018 D07

--- 2018/day7/solution.rb
+++ 2018/day7/solution.rb
@@ -40,7 +40,7 @@ end
 
 nodes = Hash.new { |h, k| h[k] = Node.new k }
 
-File.read('test.txt').scan(/^Step ([A-Z]).+step ([A-Z])/) do |s|
+File.read('input.txt').scan(/^Step ([A-Z]).+step ([A-Z])/) do |s|
   parent = nodes[s[0]]
   child = nodes[s[1]]
   parent.children << child
@@ -87,54 +87,48 @@ class Worker
 end
 
 class Processor
-  attr_reader :new_order
-
   def initialize(todo, worker_count, delay)
     @todo = todo
     @worker_count = worker_count
     @workers = (1..@worker_count).map { Worker.new delay }
-    @next_i = 0
-    @new_order = []
     @time = 0
   end
 
   def run!
-    while @todo.any? && @time < 10
+    while @todo.any?
       step!
-      printf("%d %s %s\n", @time, @workers.map { |w| w.node&.value || '.' }.join, @new_order.map(&:value).join)
+      printf("%5d: [%s] (%26s)\n", @time, @workers.map { |w| w.node&.value || '.' }.join, @todo.map(&:value).join)
       @time += 1
     end
+
+    @time - 1
   end
 
   def step!
     @workers.each do |worker|
-      next_node = get_next @next_i
-      @next_i += 1
-      worker.set!(next_node, @time) if next_node
       if worker.done? @time
-        finish!(worker.node)
+        finish! worker.node
         worker.reset!
       end
+    end
+
+    @workers.each do |worker|
+      next_node = get_next
+      worker.set!(next_node, @time) if worker.done?(@time) && next_node
     end
   end
 
   private
 
-  def get_next(i)
-    nxt = @todo[i]
-    return nil if nxt.parents.any? { |p| @todo.include? p }
-    nxt
+  def get_next
+    @todo.find { |n| n.parents.all? { |p| !@todo.include? p } && @workers.all? { |w| n != w.node } }
   end
 
   def finish!(n)
+    return unless n
     @todo.delete n
   end
 end
 
-processor = Processor.new(part1_nodes, 2, 0)
-processor.run!
-
-puts "Part 2: #{processor.new_order.map(&:value).join}"
-
-# require 'pry'
-# binding.pry
+processor = Processor.new(part1_nodes.sort_by(&:value), 5, 60)
+puts "Part 2: #{processor.run!}"
