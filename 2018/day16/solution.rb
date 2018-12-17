9ca8d4775dfa5c60a91835eab1200ea30b47eb5a
Adam Hellberg <adam.hellberg@ninetech.com>
2018-12-17 16:41:41 +0100

    Finish Ruby solution for 2018 D16

--- 2018/day16/solution.rb
+++ 2018/day16/solution.rb
@@ -23,6 +23,8 @@ OPCODES = {
 
 input_part1, input_part2 = File.read('input.txt').split("\n\n\n").map(&:strip)
 
+part2 = []
+
 part1 = input_part1
 .split("\n\n")
 .map { |b| b.lines.map { |l| l.scan(/\d+/).map(&:to_i) } }.count do |(regs, code, regs_after)|
@@ -32,7 +34,38 @@ part1 = input_part1
     OPCODES[key].call r, a, b, c
     r == regs_after
   end
+
+  if part2[op]
+    part2[op] &= matching_ops
+  else
+    part2[op] = matching_ops
+  end
+
   matching_ops.size >= 3
 end
 
 puts "Part 1: #{part1}"
+
+resolved = []
+res_count = 0
+
+part2.cycle.with_index do |ops, c|
+  i = c % part2.size
+  break if res_count == OPCODES.size
+
+  if ops.size > 1
+    part2[i] -= resolved
+    next
+  end
+
+  next if resolved[i]
+
+  resolved[i] = ops.first
+  res_count += 1
+end
+
+input_part2.scan(/(\d+) (\d+) (\d+) (\d+)/).map { |l| l.map(&:to_i) }.each do |op, a, b, c|
+  OPCODES[resolved[op]].call REGS, a, b, c
+end
+
+puts "Part 2: #{REGS[0]}"
