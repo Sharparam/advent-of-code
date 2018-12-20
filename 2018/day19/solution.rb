0217c1d64c6052f7caae8b62b5b0c35e1b36af88
Adam Hellberg <adam.hellberg@ninetech.com>
2018-12-20 11:45:14 +0100

    Add Ruby solution for 2018 D19P1

---
+++ 2018/day19/solution.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+OPCODES = {
+  addr: -> (r, a, b, c) { r[c] = r[a] + r[b] },
+  addi: -> (r, a, b, c) { r[c] = r[a] + b },
+  mulr: -> (r, a, b, c) { r[c] = r[a] * r[b] },
+  muli: -> (r, a, b, c) { r[c] = r[a] * b },
+  banr: -> (r, a, b, c) { r[c] = r[a] & r[b] },
+  bani: -> (r, a, b, c) { r[c] = r[a] & b },
+  borr: -> (r, a, b, c) { r[c] = r[a] | r[b] },
+  bori: -> (r, a, b, c) { r[c] = r[a] | b },
+  setr: -> (r, a, _, c) { r[c] = r[a] },
+  seti: -> (r, a, _, c) { r[c] = a },
+  gtir: -> (r, a, b, c) { r[c] = a > r[b] ? 1 : 0 },
+  gtri: -> (r, a, b, c) { r[c] = r[a] > b ? 1 : 0 },
+  gtrr: -> (r, a, b, c) { r[c] = r[a] > r[b] ? 1 : 0 },
+  eqir: -> (r, a, b, c) { r[c] = a == r[b] ? 1 : 0 },
+  eqri: -> (r, a, b, c) { r[c] = r[a] == b ? 1 : 0 },
+  eqrr: -> (r, a, b, c) { r[c] = r[a] == r[b] ? 1 : 0 }
+}.freeze
+
+input = File.readlines('input.txt')
+IP_REG = input.shift.split(' ').last.to_i
+PROGRAM = input.join.scan(/([a-z]+) (\d+) (\d+) (\d+)/).map { |op, a, b, c| [op.to_sym, a.to_i, b.to_i, c.to_i] }.freeze
+
+def run(regs)
+  ip = 0
+  while ip < PROGRAM.size
+    regs[IP_REG] = ip
+    print "ip=#{ip} [#{regs.join ', '}]"
+    op, a, b, c = PROGRAM[ip]
+    print " #{op} #{a} #{b} #{c}"
+    OPCODES[op].call regs, a, b, c
+    puts "[#{regs.join ', '}]"
+    ip = regs[IP_REG]
+    ip += 1
+  end
+  regs
+end
+
+puts "Part 1: #{run([0] * 6)[0]}"
+
+# Part 2 cannot be solved like this, takes too long to run
+exit
+puts "Part 2: #{run(([0] * 6).tap { |r| r[0] = 1 })[0]}"
+
+require 'pry'
+binding.pry
