d879528783341202b87c0e23a6cf4dd998e33bd4
Adam Hellberg <sharparam@sharparam.com>
2020-12-02 01:08:08 +0100

    Remove unnecessary ampersands

--- 2020/day1/solution.rb
+++ 2020/day1/solution.rb
@@ -2,5 +2,5 @@
 
 input = ARGF.readlines.map(&:to_i)
 
-puts input.combination(2).find { |p| p.sum == 2020 }.reduce(&:*)
-puts input.combination(3).find { |t| t.sum == 2020 }.reduce(&:*)
+puts input.combination(2).find { |p| p.sum == 2020 }.reduce(:*)
+puts input.combination(3).find { |t| t.sum == 2020 }.reduce(:*)
