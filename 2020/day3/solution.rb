5746e2aaa11b88bd9934e117f2e2e7eff4a5ecfe
Adam Hellberg <sharparam@sharparam.com>
2020-12-03 06:29:53 +0100

    Remove commented code

--- 2020/day3/solution.rb
+++ 2020/day3/solution.rb
@@ -3,22 +3,10 @@
 input = ARGF.readlines.map(&:strip)
 
 def count(map, step)
-  # x = 0
-  # y = 0
-  # trees = 0
-
   map.size.times.sum { |t|
     y = t * step[1]
     map[y]&.[]((t * step[0]) % map[y].size) == ?# ? 1 : 0
   }
-
-  # while y < map.size
-  #   x += step[0]
-  #   y += step[1]
-  #   trees += 1 if map[y]&.[](x % map[y].size) == ?#
-  # end
-
-  # trees
 end
 
 part1 = count input, [3, 1]
