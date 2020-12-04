9e03a6696d603d4890508f953b1c62cc2c175dda
Adam Hellberg <sharparam@sharparam.com>
2020-12-04 06:38:47 +0100

    Improve 2020 day 4 solution slightly

--- 2020/day4/solution.rb
+++ 2020/day4/solution.rb
@@ -2,40 +2,24 @@
 
 groups = ARGF.read.split("\n\n")
 
-req_fields = %w(byr iyr eyr hgt hcl ecl pid cid)
+fields = %w[byr iyr eyr hgt hcl ecl pid]
 
 passports = groups.map do |g|
-  fields = req_fields.map do |rf|
-    [rf, $1.strip] if g =~ %r{#{rf}:(\S+)}
-  end
-
-  Hash[*fields.compact.flatten]
+  Hash[*fields.filter_map { |f| [f, $1.strip] if g =~ %r{#{f}:(\S+)} }.flatten]
 end
 
-p1_req_fields = %w(byr iyr eyr hgt hcl ecl pid)
-
-part1 = passports.count { |passport|
-  p1_req_fields.all? { |rf| passport.key? rf }
-}
-
-puts part1
+puts passports.count { |p| fields.all?(&p.method(:key?)) }
 
 validators = {
-  'byr' => -> (v) { v.to_i.between?(1920, 2002) },
-  'iyr' => -> (v) { v.to_i.between?(2010, 2020) },
-  'eyr' => -> (v) { v.to_i.between?(2020, 2030) },
-  'hgt' => -> (v) {
+  'byr' => -> v { v.to_i.between?(1920, 2002) },
+  'iyr' => -> v { v.to_i.between?(2010, 2020) },
+  'eyr' => -> v { v.to_i.between?(2020, 2030) },
+  'hgt' => -> v {
     v =~ /^(\d+)(cm|in)$/ && $2 == 'cm' ? $1.to_i.between?(150, 193) : $1.to_i.between?(59, 76)
   },
-  'hcl' => -> (v) { v =~ /^#[\da-f]{6}$/ },
-  'ecl' => -> (v) { v =~ /^amb|blu|brn|gry|grn|hzl|oth$/ },
-  'pid' => -> (v) { v =~ /^\d{9}$/ }
+  'hcl' => -> v { v =~ /^#[\da-f]{6}$/ },
+  'ecl' => -> v { v =~ /^amb|blu|brn|gry|grn|hzl|oth$/ },
+  'pid' => -> v { v =~ /^\d{9}$/ }
 }
 
-part2 = passports.count { |passport|
-  p1_req_fields.all? { |rf| passport.key? rf } && validators.all? { |k, validator|
-    validator.call(passport[k])
-  }
-}
-
-puts part2
+puts passports.count { |p| validators.all? { |k, v| v.call(p[k]) } }
