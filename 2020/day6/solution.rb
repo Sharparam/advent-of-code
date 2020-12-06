4cc6faffdd1afe1953932516930123eae7636e01
Adam Hellberg <sharparam@sharparam.com>
2020-12-06 06:18:09 +0100

    Use split for reduced code

--- 2020/day6/solution.rb
+++ 2020/day6/solution.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 groups = ARGF.read.split("\n\n").map { |g|
-  g.lines.map { |l| l.strip.chars.map(&:to_sym) }
+  g.split.map { |l| l.chars.map(&:to_sym) }
 }
 
 puts groups.sum { |g| g.flatten.uniq.size }
