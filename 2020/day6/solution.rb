f2cdc5c2eb7f94795f2b41dac76294770a54bd14
Adam Hellberg <sharparam@sharparam.com>
2020-12-06 06:13:29 +0100

    Add solution for 2020 day 6

---
+++ 2020/day6/solution.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+groups = ARGF.read.split("\n\n").map { |g|
+  g.lines.map { |l| l.strip.chars.map(&:to_sym) }
+}
+
+puts groups.sum { |g| g.flatten.uniq.size }
+puts groups.sum { |g| g.reduce(:intersection).size }
