6b07cfcce806316358aaed6880a3b3ae3e66807c
Adam Hellberg <sharparam@sharparam.com>
2020-12-06 06:19:30 +0100

    Improve code size

--- 2020/day6/solution.rb
+++ 2020/day6/solution.rb
@@ -1,8 +1,5 @@
 #!/usr/bin/env ruby
 
-groups = ARGF.read.split("\n\n").map { |g|
-  g.split.map { |l| l.chars }
-}
-
+groups = ARGF.read.split("\n\n").map { |g| g.split.map(&:chars) }
 puts groups.sum { |g| g.flatten.uniq.size }
 puts groups.sum { |g| g.reduce(:&).size }
