d0abb9d798c538e8d02f657e4bba9ecd683464f5
Adam Hellberg <sharparam@sharparam.com>
2020-12-06 15:37:35 +0100

    Improve solution for 2020 day 1

--- 2020/day1/solution.rb
+++ 2020/day1/solution.rb
@@ -1,6 +1,4 @@
 #!/usr/bin/env ruby
 
 input = ARGF.readlines.map(&:to_i)
-
-puts input.combination(2).find { |p| p.sum == 2020 }.reduce(:*)
-puts input.combination(3).find { |t| t.sum == 2020 }.reduce(:*)
+puts [2, 3].map { |s| input.combination(s).find { _1.sum == 2020 }.reduce(:*) }
