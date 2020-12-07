7fde50cd2b7cae5f1d5641473d9d765949d54e96
Adam Hellberg <sharparam@sharparam.com>
2020-12-07 22:44:48 +0100

    Improve 2020 day 5

--- 2020/day5/solution.rb
+++ 2020/day5/solution.rb
@@ -1,14 +1,6 @@
 #!/usr/bin/env ruby
 
-codes = ARGF.readlines.map { |l| l.chomp.chars.map(&:to_sym) }
-
-def decode(code)
-  row = (0..127).bsearch { %i[F L R].include? code.shift }
-  col = (0..7).bsearch { [:L, nil].include? code.shift }
-  row * 8 + col
-end
-
-ids = codes.map { |c| decode c }
+ids = ARGF.readlines.map { |l| l.tr('FBLR', '0101').to_i 2 }
 min, max = ids.min, ids.max
 
 puts max
