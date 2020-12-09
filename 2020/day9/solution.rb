823bbd4c7a13d7d31e9e23023b2ba634b876027a
Adam Hellberg <sharparam@sharparam.com>
2020-12-09 06:39:51 +0100

    Shorten a variable name

--- 2020/day9/solution.rb
+++ 2020/day9/solution.rb
@@ -9,6 +9,6 @@ part1 = NUMS[SIZE..].find.with_index { |n, i|
   !NUMS[ri - SIZE...ri].combination(2).any? { _1 + _2 == n }
 }.tap { puts _1 }
 
-puts (2..).lazy.map { |size|
-  NUMS.each_cons(size).find { _1.sum == part1 }
+puts (2..).lazy.map { |s|
+  NUMS.each_cons(s).find { _1.sum == part1 }
 }.find { _1 }.then { _1.min + _1.max }
