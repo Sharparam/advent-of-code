4db7eaf296cb3a6981761115be9cc322ee4a85e7
Adam Hellberg <sharparam@sharparam.com>
2020-12-10 07:28:54 +0100

    Add solution for 2020 day 10

---
+++ 2020/day10/solution.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# fucking what
+trib_cache = [0, 1, 1]
+trib = -> n {
+  trib_cache[n] ||= trib[n - 1] + trib[n - 2] + trib[n - 3]
+}
+
+adapters = ARGF.readlines.map(&:to_i).sort
+DEVICE = adapters.max + 3
+
+diffs = Hash.new 0
+diffs[adapters.min - 0] += 1
+diffs[3] += 1
+
+adapters.each_cons(2) do |low, high|
+  diffs[high - low] += 1
+end
+
+puts diffs[1] * diffs[3]
+
+withends = [0] + adapters + [DEVICE]
+oneseqs = []
+start = withends.first
+previous = withends.first
+withends.drop(1).each do |jolt|
+  if jolt - previous > 1
+    diff = previous - start
+    oneseqs << diff + 1 if diff > 1
+    start = jolt
+  end
+  previous = jolt
+end
+
+puts oneseqs.map(&trib).reduce(:*)
