0c8c351ec3684c60d4a740528df91be62af5bc19
Adam Hellberg <sharparam@sharparam.com>
2020-12-16 06:43:29 +0100

    Add solution for 2020 day 16

---
+++ 2020/day16/solution.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+SECTIONS = ARGF.read.split "\n\n"
+
+RULES = SECTIONS[0].lines.map do |l|
+  name, ranges = l.split ?:
+  ranges = ranges.split(' or ').map { s, e = _1.split(?-).map(&:to_i); (s..e) }
+  [name, ranges]
+end
+
+TICKETS = SECTIONS[2].lines.drop(1).map { _1.split(?,).map(&:to_i) }
+
+INVALID_VALUES = TICKETS.flatten.select do |v|
+  RULES.all? { |_, r| !r.any? { _1.include? v } }
+end
+
+puts INVALID_VALUES.sum
+
+VALID_TICKETS = TICKETS.select do |t|
+  t.all? { |v| RULES.any? { |_, r| r.any? { _1.include? v } } }
+end
+
+indices_valid_for = {}
+
+(0..RULES.size - 1).each do |index|
+  indices_valid_for[index] = RULES.select do |_, ranges|
+    VALID_TICKETS.all? do |ticket|
+      ranges.any? { _1.include? ticket[index] }
+    end
+  end.map { _1[0] }
+end
+
+while indices_valid_for.any? { _2.size > 1 }
+  singles = indices_valid_for.select { _2.size == 1 }.map { |_, f| f[0] }
+  indices_valid_for.select { _2.size > 1 }.each do |_, v|
+    v.reject! { singles.include? _1 }
+  end
+end
+
+MY_TICKET = SECTIONS[1].lines[1].split(?,).map.with_index do |value, index|
+  [indices_valid_for[index][0], value.to_i]
+end
+
+puts MY_TICKET.select { _1[0].start_with? 'departure' }.map { _2 }.reduce(:*)
