8ca276d83e6bb9d1d2b85dc9d2fe4f94edcedd21
Adam Hellberg <sharparam@sharparam.com>
2020-12-18 21:52:01 +0100

    Make solution for 2020 day 18 more evil
    After inspiration from @itsbth.

--- 2020/day18/solution.rb
+++ 2020/day18/solution.rb
@@ -1,11 +1,9 @@
 #!/usr/bin/env ruby
 
 class Integer
-  def - _; self * _; end
-  def / _; self + _; end
+  def - _; self * _ end
+  def / _; self + _ end
+  def & _; self + _ end
 end
 
-INPUT = ARGF.readlines.map { _1.gsub(?*, ?-) }
-
-puts INPUT.sum { eval _1 }
-puts INPUT.sum { eval _1.gsub(?+, ?/) }
+puts eval (ARGF.read.gsub(?*,?-).split(?\n)*?&).tap{puts eval _1}.gsub ?+,?/
