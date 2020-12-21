0cf22fb4314d14dd30cbbcb4152862f2eb0019d0
Adam Hellberg <sharparam@sharparam.com>
2020-12-21 06:52:52 +0100

    Add solution for 2020 day 21

---
+++ 2020/day21/solution.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'set'
+
+FOODS = ARGF.readlines.map do |f|
+  f =~ /((?:\w+ ?)+)\(contains ((?:\w+(?:, )?)+)\)/
+  [$1.split, $2.split(?,)].map { |v| v.map { _1.strip.to_sym } }
+end
+
+ALL_INGREDIENTS = FOODS.flat_map(&:first)
+INGREDIENTS = ALL_INGREDIENTS.to_set
+ALLERGENS = FOODS.flat_map { _2 }.to_set
+
+ALLERGEN_INGREDIENTS = Hash[ALLERGENS.map do |a|
+  [a, FOODS.select { _2.include? a }.map(&:first).reduce(:intersection)]
+end]
+
+puts (ALL_INGREDIENTS - ALLERGEN_INGREDIENTS.values.flatten).size
+
+while ALLERGEN_INGREDIENTS.values.any? { _1.count > 1 }
+  singles = ALLERGEN_INGREDIENTS.values.select { _1.count == 1 }.flatten.to_set
+  ALLERGEN_INGREDIENTS.values.select { _1.count > 1 }.each do |v|
+    v.reject! { singles.include? _1 }
+  end
+end
+
+puts ALLERGEN_INGREDIENTS.sort_by(&:first).map { _2.first }.join ?,
