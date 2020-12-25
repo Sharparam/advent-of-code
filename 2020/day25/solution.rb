600a8b072db8934ead7deeb7e6d28968dd24d928
Adam Hellberg <sharparam@sharparam.com>
2020-12-25 17:37:36 +0100

    Improve 2020 day 25

--- 2020/day25/solution.rb
+++ 2020/day25/solution.rb
@@ -1,21 +1,6 @@
 #!/usr/bin/env ruby
 
-DIVISOR = 20201227
-TRANSFORM_MEM = {}
+DIV, v = 20201227, 1
+CARD, DOOR = ARGF.readlines.map(&:to_i)
 
-def transform(count, subject: 7, value: 1)
-  prev = [count - 1, subject]
-  if TRANSFORM_MEM.key? prev
-    value = (TRANSFORM_MEM[prev] * subject) % DIVISOR
-  else
-    count.times { value = (value * subject) % DIVISOR }
-  end
-
-  TRANSFORM_MEM[[count, subject]] = value
-end
-
-CARD_PUBKEY, DOOR_PUBKEY = ARGF.readlines.map(&:to_i)
-
-CARD_LOOP_SIZE = (1..).find { transform(_1) == CARD_PUBKEY }
-
-puts transform CARD_LOOP_SIZE, subject: DOOR_PUBKEY
+puts (1..).find { (v = (v * 7) % DIV) == CARD }.times.reduce(1) { |a, _| (a * DOOR) % DIV }
