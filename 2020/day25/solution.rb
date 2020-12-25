67086557f3a58728c2b4d3fec9ac207e1161c3cd
Adam Hellberg <sharparam@sharparam.com>
2020-12-25 17:46:38 +0100

    Further improve 2020 day 25

--- 2020/day25/solution.rb
+++ 2020/day25/solution.rb
@@ -3,4 +3,4 @@
 DIV, v = 20201227, 1
 CARD, DOOR = ARGF.readlines.map(&:to_i)
 
-puts (1..).find { (v = (v * 7) % DIV) == CARD }.times.reduce(1) { |a, _| (a * DOOR) % DIV }
+puts DOOR.pow((1..).find { (v = (v * 7) % DIV) == CARD }, DIV)
