502c2dac7e801a2ac1eb234fff50c947f3565afa
Adam Hellberg <sharparam@sharparam.com>
2021-11-01 18:00:00 +0100

    Add solution for 2017 day 12

---
+++ 2017/day12/solution.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'set'
+
+input = ARGF.readlines
+
+map = {}
+
+input.each do |line|
+  next unless line =~ /(\d+)[^\d]+([\d, ]+)/
+  reachable = map[$1.to_i] ||= Set.new
+  $2.split(',').map(&:to_i).each { |t| reachable.add t }
+end
+
+visited = Set.new
+tovisit = [0]
+while tovisit.any?
+  current = tovisit.pop
+  visited.add current
+  reachable = map[current].reject { |n| visited.include? n }
+  tovisit += reachable.to_a
+end
+
+puts visited.size
+
+puts 1 + (map.keys.count { |start|
+  if visited.include? start
+    false
+  else
+    tovisit = [start]
+    while tovisit.any?
+      current = tovisit.pop
+      visited.add current
+      reachable = map[current].reject { |n| visited.include? n }
+      tovisit += reachable.to_a
+    end
+
+    true
+  end
+})
