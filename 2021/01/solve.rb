d06fe0d1e93e95a840ddbc8e343581c0a187706f
Adam Hellberg <sharparam@sharparam.com>
2021-12-01 06:17:57 +0100

    Apply JuanPotato optimization

--- 2021/01/solve.rb
+++ 2021/01/solve.rb
@@ -2,23 +2,5 @@
 # frozen_string_literal: true
 
 nums = ARGF.readlines.map(&:to_i)
-
-current = nums.max + 1
-count = 0
-
-nums.each do |num|
-  count += 1 if num > current
-  current = num
-end
-
-puts count
-
-current = nums.max + 1
-count = 0
-
-nums.each_cons(3) do |g|
-  count += 1 if g.sum > current
-  current = g.sum
-end
-
-puts count
+puts nums.each_cons(2).count { |a, b| a < b }
+puts nums.each_cons(3).each_cons(2).count { |a, b| a.sum < b.sum }
