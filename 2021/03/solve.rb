4edf02cfb634faaf5a64c5a9e23ebed7a529487c
Adam Hellberg <sharparam@sharparam.com>
2021-12-03 06:47:09 +0100

    Improve 2021 day 3 Ruby solution

--- 2021/03/solve.rb
+++ 2021/03/solve.rb
@@ -3,14 +3,14 @@
 
 input = ARGF.readlines.map(&:strip)
 
-puts input.first.size.times.map { |i| input.map { _1[i] }.tally }.map { |f| f.minmax_by { _2 } }.map { _1.map(&:first) }.reduce(['', '']) { [_1[0] + _2[0], _1[1] + _2[1]] }.map { _1.to_i(2) }.reduce(:*)
+puts input.first.size.times.map { |i| input.map { _1[i] }.tally }.map { |f| f.minmax_by { _2 } }.map { _1.map(&:first) }.transpose.map { _1.join.to_i(2) }.reduce(:*)
 
 oxy_nums = input.dup
 index = 0
 while oxy_nums.size > 1
   consider = oxy_nums.map { |n| n[index] }
   t = consider.tally
-  m = t.max_by { |k, v| v + (k == '1' ? 0.5 : 0) }.first
+  m = t.max_by { |k, v| [v, k] }.first
   oxy_nums.reject! { |n| n[index] != m }
   index += 1
 end
@@ -22,7 +22,7 @@ index = 0
 while co2_nums.size > 1
   consider = co2_nums.map { |n| n[index] }
   t = consider.tally
-  m = t.min_by { |k, v| v + (k == '1' ? 0.5 : 0) }.first
+  m = t.min_by { |k, v| [v, k] }.first
   co2_nums.reject! { |n| n[index] != m }
   index += 1
 end
