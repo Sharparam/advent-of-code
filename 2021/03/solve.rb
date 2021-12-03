3fe214ce2ea74a6594f204b429a61cd7ae0e4b43
Adam Hellberg <sharparam@sharparam.com>
2021-12-03 06:53:28 +0100

    Improve 2021 day 3 Ruby solution

--- 2021/03/solve.rb
+++ 2021/03/solve.rb
@@ -5,28 +5,18 @@ input = ARGF.readlines.map(&:strip)
 
 puts input.first.size.times.map { |i| input.map { _1[i] }.tally }.map { |f| f.minmax_by { _2 } }.map { _1.map(&:first) }.transpose.map { _1.join.to_i(2) }.reduce(:*)
 
-oxy_nums = input.dup
-index = 0
-while oxy_nums.size > 1
-  consider = oxy_nums.map { |n| n[index] }
-  t = consider.tally
-  m = t.max_by { |k, v| [v, k] }.first
-  oxy_nums.reject! { |n| n[index] != m }
-  index += 1
+oxy = input.dup
+input.first.size.times do |i|
+  tally = oxy.map { _1[i] }.tally
+  m = tally.max_by { |k, v| [v, k] }.first
+  oxy.select! { _1[i] == m }
 end
 
-oxy = oxy_nums.first.to_i(2)
-
-co2_nums = input.dup
-index = 0
-while co2_nums.size > 1
-  consider = co2_nums.map { |n| n[index] }
-  t = consider.tally
-  m = t.min_by { |k, v| [v, k] }.first
-  co2_nums.reject! { |n| n[index] != m }
-  index += 1
+co2 = input.dup
+input.first.size.times do |i|
+  tally = co2.map { _1[i] }.tally
+  m = tally.min_by { |k, v| [v, k] }.first
+  co2.select! { _1[i] == m }
 end
 
-co2 = co2_nums.first.to_i(2)
-
-puts oxy * co2
+puts oxy.first.to_i(2) * co2.first.to_i(2)
