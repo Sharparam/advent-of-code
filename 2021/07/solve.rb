28acf748912973552a43d12fbaad86743d25ec18
Adam Hellberg <sharparam@sharparam.com>
2021-12-07 06:47:23 +0100

    Apply JuanPotato optimization

--- 2021/07/solve.rb
+++ 2021/07/solve.rb
@@ -3,6 +3,7 @@
 
 positions = ARGF.read.split(',').map(&:to_i)
 min, max = positions.minmax
+costs = (1..(max - min + 1)).inject([0]) { _1 << _1.last + _2 }
 
 puts (min..max).map { |pos| positions.sum { |p| (p - pos).abs } }.min
-puts (min..max).map { |pos| positions.sum { |p| ((p - pos).abs).downto(1).sum } }.min
+puts (min..max).map { |pos| positions.sum { |p| costs[(p - pos).abs] } }.min
