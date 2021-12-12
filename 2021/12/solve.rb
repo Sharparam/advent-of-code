dca7206fee2c466497cba0fbebc8dcf55bffcd3e
Adam Hellberg <sharparam@sharparam.com>
2021-12-12 07:50:29 +0100

    Apply @itsbth optimization

--- 2021/12/solve.rb
+++ 2021/12/solve.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'matrix'
 require 'set'
-require 'pp'
 
 MAZE = {}
 ARGF.readlines.map { _1.strip.split('-') }.each do |a, b|
@@ -14,49 +12,15 @@ ARGF.readlines.map { _1.strip.split('-') }.each do |a, b|
   MAZE[b] << a
 end
 
-def search(current, visited, paths, path)
-  visited << current if current =~ /^[a-z]+$/
-  path << current
-  if current == 'end'
-    paths << path
-    return
-  end
-  avail = MAZE[current].reject { visited.include? _1 }
-  return if avail.empty?
-  avail.each do |a|
-    search(a, visited.dup, paths, path.dup)
+def solve(current, visited, twice)
+  visited.add current if current =~ /^[a-z]+$/
+  return 1 if current == 'end'
+  MAZE[current].sum do |node|
+    seen = visited.include? node
+    next 0 if seen && (twice || node == 'start' || node == 'end')
+    solve(node, visited.dup, twice || seen)
   end
 end
 
-def search2(current, visited, paths, path, twice)
-  is_lower = current =~ /^[a-z]+$/
-  visited[current] += 1 if is_lower
-  path << current
-  if current == 'end'
-    paths << path
-    return
-  end
-  MAZE[current].each do |node|
-    next if node == 'start'
-    next search2(node, visited.dup, paths, path.dup, twice.dup) if node == 'end' || node =~ /^[A-Z]+$/
-    next search2(node, visited.dup, paths, path.dup, twice.dup) if twice[node] == true && visited[node] < 2
-    if twice.values.any?(true) || twice[node] == false
-      search2(node, visited.dup, paths, path.dup, twice.dup) if visited[node] < 1
-      next
-    end
-    first = twice.dup
-    second = twice.dup
-    first[node] = true
-    second[node] = false
-    search2(node, visited.dup, paths, path.dup, first)
-    search2(node, visited.dup, paths, path.dup, second)
-  end
-end
-
-paths = []
-search('start', Set.new, paths, [])
-puts paths.size
-
-paths2 = []
-search2('start', Hash.new(0), paths2, [], {})
-puts paths2.uniq.size # TODO: Figure out why we get duplicate paths
+puts solve('start', Set.new, true)
+puts solve('start', Set.new, false)
