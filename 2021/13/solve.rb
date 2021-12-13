46cbbaa4dbbbd6b61bdadf2f688e9adae986985e
Adam Hellberg <sharparam@sharparam.com>
2021-12-13 07:11:44 +0100

    Improve 2021 day 13 Ruby solution

--- 2021/13/solve.rb
+++ 2021/13/solve.rb
@@ -1,17 +1,16 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'matrix'
 require 'set'
 
 input, instructions = ARGF.read.split("\n\n")
-grid = input.lines.map { Vector[*_1.split(',').map(&:to_i)] }.to_set
-instructions = instructions.scan(/(x|y)=(\d+)/).map { |a, n| [a == 'x' ? 0 : 1, n.to_i] }
+grid = input.lines.map { [*_1.split(',').map(&:to_i)] }.to_set
+instructions = instructions.scan(/(x|y)=(\d+)/).map { [_1 == 'x' ? 0 : 1, _2.to_i] }
 
 def vis(grid)
   max_x = grid.map(&:first).max
   max_y = grid.map { _1[1] }.max
-  puts (0..max_y).map { |y| (0..max_x).map { grid.include?(Vector[_1, y]) ? '##' : '..' }.join }
+  puts (0..max_y).map { |y| (0..max_x).map { grid.include?([_1, y]) ? '##' : '..' }.join }
 end
 
 def fold(grid, axis, pos)
