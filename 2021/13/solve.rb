57227130d09468d93594debd3fcbf55edb66cb1a
Adam Hellberg <sharparam@sharparam.com>
2021-12-13 07:17:19 +0100

    Make visualization for 2021 day 13 nicer

--- 2021/13/solve.rb
+++ 2021/13/solve.rb
@@ -10,7 +10,7 @@ instructions = instructions.scan(/(x|y)=(\d+)/).map { [_1 == 'x' ? 0 : 1, _2.to_
 def vis(grid)
   max_x = grid.map(&:first).max
   max_y = grid.map { _1[1] }.max
-  puts (0..max_y).map { |y| (0..max_x).map { grid.include?([_1, y]) ? '##' : '..' }.join }
+  puts (0..max_y).map { |y| (0..max_x).map { grid.include?([_1, y]) ? '##' : '  ' }.join }
 end
 
 def fold(grid, axis, pos)
