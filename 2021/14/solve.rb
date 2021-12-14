deea1e6349bcaa36f48e6c8b8c20bee3742633a2
Adam Hellberg <sharparam@sharparam.com>
2021-12-14 07:25:58 +0100

    Add Ruby solution for 2021 day 14

---
+++ 2021/14/solve.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+TEMPLATE = ARGF.readline.strip
+FIRST, LAST = [TEMPLATE[0], TEMPLATE[-1]]
+PAIRS = TEMPLATE.chars.each_cons(2).tally
+ARGF.readline
+RULES = Hash[ARGF.read.scan(/(..) -> (.)/).map { [_1.chars, _2] }].freeze
+
+def step(pairs)
+  result = Hash.new(0)
+  pairs.each do |pair, count|
+    if RULES.key? pair
+      char = RULES[pair]
+      result[[pair[0], char]] += count
+      result[[char, pair[1]]] += count
+    else
+      result[pair] += count
+    end
+  end
+  result
+end
+
+def count(pairs)
+  pairs.map { |(a, b), n| [[a, n], [b, n]] }.flatten(1).reduce(Hash.new(0)) do |h, (c, n)|
+    h[c] += n
+    h
+  end.tap do |h|
+    h[FIRST] += 1
+    h[LAST] += 1
+    h.keys.each { h[_1] /= 2 }
+  end
+end
+
+puts count(10.times.reduce(PAIRS) { step(_1) }).values.minmax.reverse.reduce(:-)
+puts count(40.times.reduce(PAIRS) { step(_1) }).values.minmax.reverse.reduce(:-)
