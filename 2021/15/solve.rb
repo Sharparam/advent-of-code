6ba21b9e0a4b792d9345f0c87550d9bcef667726
Adam Hellberg <sharparam@sharparam.com>
2021-12-15 07:26:14 +0100

    Improve 2021 day 15 Ruby solution

--- 2021/15/solve.rb
+++ 2021/15/solve.rb
@@ -4,35 +4,32 @@
 require_relative '../../lib/astar'
 require_relative '../../lib/grid'
 
-input = ARGF.readlines.map(&:strip).map { _1.split('').map(&:to_i) }
-WIDTH = input[0].size
-HEIGHT = input.size
+INPUT = ARGF.readlines.map(&:strip).map { _1.split('').map(&:to_i) }
+WIDTH = INPUT[0].size
+HEIGHT = INPUT.size
 START = Vector[0, 0]
 GOAL = Vector[WIDTH - 1, HEIGHT - 1]
 FULL_WIDTH = WIDTH * 5
 FULL_HEIGHT = HEIGHT * 5
 FULL_GOAL = Vector[FULL_WIDTH - 1, FULL_HEIGHT - 1]
 
-grid = GridWithWeights.new(WIDTH, HEIGHT)
-full_grid = GridWithWeights.new(FULL_WIDTH, FULL_HEIGHT)
+grid = GridWithWeights.new(FULL_WIDTH, FULL_HEIGHT)
 
-input.each_with_index do |row, y|
+INPUT.each_with_index do |row, y|
   row.each_with_index do |weight, x|
     grid.add_weight Vector[x, y], weight
-    full_grid.add_weight Vector[x, y], weight
     (1..4).each do |n|
       (0..4).each do |n2|
-        new_weight = weight + n + n2
-        new_weight -= 9 if new_weight > 9
-        full_grid.add_weight Vector[x + (WIDTH * n), y + (HEIGHT * n2)], new_weight
-        full_grid.add_weight Vector[x + (WIDTH * n2), y + (HEIGHT * n)], new_weight
+        new_weight = (weight + n + n2 - 1) % 9 + 1
+        grid.add_weight Vector[x + (WIDTH * n), y + (HEIGHT * n2)], new_weight
+        grid.add_weight Vector[x + (WIDTH * n2), y + (HEIGHT * n)], new_weight
       end
     end
   end
 end
 
 _, costs = AStar.find_path(grid, START, GOAL)
-_, full_costs = AStar.find_path(full_grid, START, FULL_GOAL)
+_, full_costs = AStar.find_path(grid, START, FULL_GOAL)
 
 puts costs[GOAL]
 puts full_costs[FULL_GOAL]
