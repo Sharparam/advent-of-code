719119347a2cdb3f5ec608cd41390cf4245d3618
Adam Hellberg <sharparam@sharparam.com>
2022-12-03 15:35:11 +0100

    Add Ruby solution for 2022 day 3

---
+++ 2022/03/solve.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+PRIOS = Hash[(?a..?z).to_a.concat((?A..?Z).to_a).map.with_index { |l, i| [l, i + 1] }]
+
+contents = ARGF.readlines.map { |l| l.chomp.chars.map { |c| PRIOS[c] } }
+
+puts contents.map { |l| l.each_slice(l.size / 2).reduce(&:intersection)[0] }.sum
+puts contents.each_slice(3).map { |g| g.reduce(&:intersection)[0] }.sum
