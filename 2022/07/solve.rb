f31b01565460aba1f27b3598abfac32d0e0f9b3a
Adam Hellberg <sharparam@sharparam.com>
2022-12-07 10:34:49 +0100

    Add Ruby solution for 2022 day 7

---
+++ 2022/07/solve.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+input = ARGF.readlines.map(&:chomp)
+
+files = Hash.new(0)
+
+cwd = []
+
+input.each do |line|
+  case line
+  when /^\$ cd \/$/
+    cwd.clear
+  when /^\$ cd \.\.$/
+    cwd.pop
+  when /^\$ cd (.+)/
+    cwd.push $1
+  when /^(\d+) (.+)$/
+    files["/#{cwd.join ?/}#{cwd.empty? ? '' : '/'}#{$2}"] = $1.to_i
+  end
+end
+
+directories = Hash.new(0)
+
+def record_size(h, path, size)
+  dir = File.dirname(path)
+  h[dir] += size
+
+  return if dir == '/'
+
+  record_size h, dir, size
+end
+
+files.each { |n, s| record_size directories, n, s }
+
+puts directories.map { _2 }.select { _1 <= 100000 }.sum
+
+min = directories[?/] - 40000000
+puts directories.map { _2 }.select { _1 >= min }.sort.first
