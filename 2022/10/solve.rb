9a2aa37598c2e10407c65f049ff8a8b18f713458
Adam Hellberg <sharparam@sharparam.com>
2022-12-10 14:41:13 +0100

    Make solution smaller

--- 2022/10/solve.rb
+++ 2022/10/solve.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'pry'
-
 cycles = [1]
 x = 1
 
@@ -11,19 +9,15 @@ ARGF.readlines.each do |l|
   when /^noop/
     cycles.push x
   when /^addx (-?\d+)/
-    v = $1.to_i
-    cycles += [x, x+= v]
+    cycles += [x, x+= $1.to_i]
   end
 end
 
 puts 19.step(cycles.size, 40).map { cycles[_1] * (_1 + 1) }.sum
 
 crt = 240.times.map do |i|
-  h = i % 40
   v = cycles[i]
-  pixel = (v - 1..v + 1)
-  lit = pixel.include?(h)
-  lit ? '#' : ':'
+  (v - 1..v + 1).include?(i % 40) ? '#' : ':'
 end
 
 puts crt.each_slice(40).map(&:join).join("\n")
