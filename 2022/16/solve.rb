cd7d7c9edc01424b698f78d119c3313bfdd4bc5e
Adam Hellberg <sharparam@sharparam.com>
2022-12-16 18:02:52 +0100

    Add partial solution for 2022 day 16 part 2
    Code produces correct answer for real input but does not work for the
    example input.

--- 2022/16/solve.rb
+++ 2022/16/solve.rb
@@ -20,9 +20,8 @@ def solve(valves, memory, mins, current, score, visited, opened)
   key = [mins, current, score]
   return memory[key] if memory.key?(key)
 
-  return score if mins <= 0
-
-  return score if HAS_FLOW.all? { opened.include? _1 }
+  return [score, opened] if mins <= 0
+  return [score, opened] if HAS_FLOW.all? { opened.include? _1 }
 
   visited = visited.dup
   visited.add current
@@ -31,23 +30,42 @@ def solve(valves, memory, mins, current, score, visited, opened)
   rate = valve[:rate]
 
   max = score
+  max_opened = opened
 
   tovisit = valve[:tunnels]
   tovisit.each do |tunnel|
-    simple = solve(valves, memory, mins - 1, tunnel, score, visited, opened)
-    max = simple if simple > max
+    simple_max, simple_o = solve(valves, memory, mins - 1, tunnel, score, visited, opened)
+    if simple_max > max
+      max = simple_max
+      max_opened = simple_o
+    end
     if rate > 0 && !opened.include?(current)
       added = rate * (mins - 1)
       new_opened = opened.dup
       new_opened.add current
-      complex = solve(valves, memory, mins - 2, tunnel, score + added, visited, new_opened)
-      max = complex if complex > max
+      complex_max, complex_o = solve(valves, memory, mins - 2, tunnel, score + added, visited, new_opened)
+      if complex_max > max
+        max = complex_max
+        max_opened = complex_o
+      end
     end
   end
 
-  memory[key] = max
+  memory[key] = [max, max_opened]
 
-  return max
+  return [max, max_opened]
 end
 
-puts solve(valves, {}, 30, :AA, 0, Set.new, Set.new)
+part1, opened = solve(valves, {}, 30, :AA, 0, Set.new, Set.new)
+
+puts part1
+
+part2_human, opened2_human = solve(valves, {}, 26, :AA, 0, Set.new, Set.new)
+opened2_human.each { valves[_1][:rate] = 0 }
+part2_elephant, opened2_elephant = solve(valves, {}, 26, :AA, 0, Set.new, Set.new)
+
+STDERR.puts "PART 2:"
+STDERR.puts "Human score: #{part2_human} (opened #{opened2_human})"
+STDERR.puts "Elephant score: #{part2_elephant} (opened #{opened2_elephant})"
+
+puts part2_human + part2_elephant
