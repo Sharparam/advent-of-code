3f1930dc77ed6d34172ecd2d2f88753edbc11609
Adam Hellberg <sharparam@sharparam.com>
2022-12-17 09:48:18 +0100

    Solve part 2 of 2022 day 17

--- 2022/17/solve.rb
+++ 2022/17/solve.rb
@@ -58,8 +58,18 @@ SHAPES = [
 grid = (0..MAX_X).map { V[_1, 0] }.to_set
 movement_index = 0
 
-2022.times do |shape_index|
-  shape = SHAPES[shape_index % SHAPES.size]
+checks = {}
+
+ROUNDS = 1_000_000_000_000
+
+i = 0
+
+part2_diff = 0
+found_cycle = false
+
+while i < ROUNDS
+  s_i = i % SHAPES.size
+  shape = SHAPES[s_i]
   shape_height = shape.map { _1[1] }.max + 1
   shape_width = shape.map { _1[0] }.max + 1
   highest_y = grid.map { _1[1] }.max # || 0
@@ -69,7 +79,8 @@ movement_index = 0
   shape = shape.map { _1 + shape_start }
 
   loop do
-    movement = MOVEMENTS[movement_index % MOVEMENTS.size]
+    m_i = movement_index % MOVEMENTS.size
+    movement = MOVEMENTS[m_i]
     movement_index += 1
     new_shape = shape.map { _1 + movement }
     unless new_shape.any? { grid.include?(_1) || _1[0] < 0 || _1[0] > MAX_X }
@@ -85,5 +96,48 @@ movement_index = 0
 
   shape.each { grid.add _1 }
 
-  puts grid.map { _1[1] }.max if shape_index == 2021
+  new_highest_y = grid.map { _1[1] }.max
+
+  # Part 1
+  puts new_highest_y if i == 2021
+
+  if i > 2021 && !found_cycle
+    y = new_highest_y
+    top = (0..MAX_X).map { |x| grid.include?(V[x, y]) ? '#' : '.' }
+    hash = [s_i, movement_index % MOVEMENTS.size, *top].hash
+
+    if checks.key?(hash)
+      found_cycle = true
+      data = checks[hash]
+      previous_height = data[:height]
+      previous_i = data[:i]
+      cycle_size = i - previous_i
+      height_diff = y - previous_height
+
+      STDERR.puts "=== CYCLE DETECTED ==="
+      STDERR.puts "Current i: #{i}; Previous i: #{previous_i}; DIFF: #{cycle_size}"
+      STDERR.puts "Current y: #{y}; Previous y: #{previous_height}; DIFF: #{height_diff}"
+
+      remaining_i = ROUNDS - i
+      remaining_cycles = remaining_i / cycle_size
+
+      STDERR.puts "Remaining rocks: #{remaining_i}; Remaining cycles: #{remaining_cycles}"
+
+      add_i = cycle_size * remaining_cycles
+      add_height = remaining_cycles * height_diff
+      i += add_i
+      part2_diff = add_height
+
+      STDERR.puts "New i is #{i}"
+    else
+      checks[hash] = { height: y, i: i }
+    end
+  end
+
+  i += 1
 end
+
+max_y = grid.map { _1[1] }.max
+
+# Part 2
+puts max_y + part2_diff
