f99e737b1f85ac5df73906e3a9efbe91ace4c4cd
Adam Hellberg <sharparam@sharparam.com>
2022-12-20 17:41:12 +0100

    Add Ruby solution for 2022 day 20

---
+++ 2022/20/solve.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+NUMS = ARGF.readlines.map(&:to_i).freeze
+LEN = NUMS.size
+MOD = LEN - 1
+
+def mix(nums, indices)
+  LEN.times do |original_i|
+    current_i = indices.index original_i
+    indices.delete_at current_i
+    indices.insert (current_i + nums[original_i]) % MOD, original_i
+  end
+end
+
+def solve(nums, n)
+  indices = LEN.times.to_a
+  n.times { mix(nums, indices) }
+  zero_i = indices.index nums.index 0
+  [1000, 2000, 3000].sum { |n| nums[indices[(zero_i + n) % LEN]] }
+end
+
+puts solve(NUMS, 1)
+
+KEY = 811589153
+NUMS2 = NUMS.map { _1 * KEY }
+
+puts solve(NUMS2, 10)
