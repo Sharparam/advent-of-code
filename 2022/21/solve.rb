f259182d403aea3743df0b3de611e088b756a120
Adam Hellberg <sharparam@sharparam.com>
2022-12-21 17:52:27 +0100

    Solve 2022 day 21 part 2 in Ruby

--- 2022/21/solve.rb
+++ 2022/21/solve.rb
@@ -1,6 +1,17 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-eval ARGF.read.gsub(/(\w+): (.+)\n?/, 'def \1(); \2; end;')
+eval(ARGF.read.gsub(/(?<=\d)\b/, '.0').gsub(/(\w+): (.+)\n?/) do
+  if $1 == 'root'
+    "def #{$1}; #{$2}; end; def #{$1}2; #{$2.scan(/\w+/).join(' - ')}; end;"
+  else
+    "def #{$1}; #{$2}; end;"
+  end
+end)
 
-puts root()
+puts root().to_i
+
+part2 = (0..100_000_000_000_000).bsearch { |n| eval("def humn; #{n}; end"); root2() <=> 0 }
+part2 = (0..100_000_000_000_000).bsearch { |n| eval("def humn; #{n}; end"); 0 <=> root2() } unless part2
+
+puts part2
