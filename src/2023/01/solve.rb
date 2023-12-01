028c9e710fa6cc3b7c22ad987c9b9a4b6b4b6dae
Adam Hellberg <sharparam@sharparam.com>
2023-12-01 07:02:43 +0100

    Improve solution for 2023 day 1

--- src/2023/01/solve.rb
+++ src/2023/01/solve.rb
@@ -1,35 +1,22 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-cals = ARGF.read.lines
-digits = cals.map { |cal| cal.scan(/\d/).to_a.map(&:to_i) }
-pairs = digits.map { |digits| [digits.first, digits.last] }
-nums = pairs.map { |pair| "#{pair.first}#{pair.last}".to_i }
-puts nums.sum
+lines = ARGF.read.lines
 
-re = /\d|one|two|three|four|five|six|seven|eight|nine/
-rre = /\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin/
+puts lines.sum { _1.scan(/\d/).then { |m| [m.first, m.last].join.to_i } }
 
-digmap = {
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9
+re = /(?=(\d|one|two|three|four|five|six|seven|eight|nine))/
+map = {
+  'one' => 1,
+  'two' => 2,
+  'three' => 3,
+  'four' => 4,
+  'five' => 5,
+  'six' => 6,
+  'seven' => 7,
+  'eight' => 8,
+  'nine' => 9
 }
-revdigmap = Hash[digmap.map { |k, v| [k.to_s.reverse.to_sym, v] }]
 
-nums = cals.map do |line|
-  firsts = line.scan(re).map { |d| digmap[d.to_sym] || d.to_i }
-  first = firsts.first
-  seconds = line.reverse.scan(rre).map { |d| revdigmap[d.to_sym] || d.to_i }
-  second = seconds.first
-
-  "#{first}#{second}".to_i
-end
-
-p nums.sum
+digits = lines.map { _1.scan(re).flatten.map { |d| map[d] || d.to_i } }
+puts digits.sum { |d| [d.first, d.last].join.to_i }
