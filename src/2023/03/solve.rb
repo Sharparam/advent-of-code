59caf63b96165700d9101f60a10d4f56a3dc48c8
Adam Hellberg <sharparam@sharparam.com>
2023-12-03 06:55:51 +0100

    Fix code

--- src/2023/03/solve.rb
+++ src/2023/03/solve.rb
@@ -27,7 +27,7 @@ ARGF.read.lines.each_with_index do |line, row|
     when '.'
       if num.any?
         number = num.join.to_i
-        num.clear
+        num = []
         start = Vector[x, row]
         stop = Vector[col - 1, row]
         numbers << { id: id += 1, value: number, start: start, stop: stop }
@@ -36,7 +36,14 @@ ARGF.read.lines.each_with_index do |line, row|
       x = col if num.empty?
       num << char
     else
-      symbols << Vector[col, row]
+      if num.any?
+        number = num.join.to_i
+        num = []
+        start = Vector[x, row]
+        stop = Vector[col - 1, row]
+        numbers << { id: id += 1, value: number, start: start, stop: stop }
+      end
+      symbols << { value: char, pos: Vector[col, row] }
     end
   end
   if num.any?
@@ -47,15 +54,29 @@ ARGF.read.lines.each_with_index do |line, row|
   end
 end
 
+puts "Total sum: #{numbers.sum { |n| n[:value] }}"
+
 part1 = 0
 
 seen = Set.new
 numbers.each do |number|
-  if !seen.include?(number[:id]) && symbols.any? { |symbol| symbol.around?(number[:start], number[:stop]) }
-    # puts "Adding #{number}"
+  if !seen.include?(number[:id]) && symbols.any? { |symbol| symbol[:pos].around?(number[:start], number[:stop]) }
     part1 += number[:value]
     seen.add number[:id]
   end
 end
 
 puts part1
+
+gears = symbols.select { |s| s[:value] == '*' }
+
+part2 = 0
+
+gears.each do |gear|
+  nums = numbers.select { |n| gear[:pos].around?(n[:start], n[:stop]) }
+  if nums.size == 2
+    part2 += nums.reduce(1) { |a, e| a * e[:value] }
+  end
+end
+
+puts part2
