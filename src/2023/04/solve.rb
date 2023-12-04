3d235be89156b3946f395b99a517246b19fa1b77
Adam Hellberg <sharparam@sharparam.com>
2023-12-04 06:41:53 +0100

    Add Ruby solution for 2023 day 4

---
+++ src/2023/04/solve.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+cards = Hash[ARGF.read.lines.map do |line|
+  t, l = line.split(':')
+  id = t.scan(/\d+/)[0].to_i
+  w, n = l.split('|').map { _1.split(' ').map(&:to_i) }
+  [id, [w, n]]
+end]
+
+win_count = {}
+
+puts cards.sum { |i, (w, n)|
+  c = n.count { w.include? _1 }
+  win_count[i] = c
+  if c == 0
+    0
+  else
+    s = 1
+    (c - 1).times { s *= 2 }
+    s
+  end
+}
+
+total_count = Hash[win_count.keys.map { [_1, 1] }]
+ids = cards.keys
+
+ids.each do |id|
+  next if win_count[id] == 0
+  c_ids = ids[id..(id + win_count[id] - 1)]
+  e = (total_count[id] * win_count[id]) / c_ids.size
+  c_ids.each { total_count[_1] += e }
+end
+
+# p total_count
+
+puts total_count.values.sum
