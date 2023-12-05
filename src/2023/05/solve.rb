460d455e1aafdc33e61e0b7d4f470715d21f0bcf
Adam Hellberg <sharparam@sharparam.com>
2023-12-05 08:33:57 +0100

    Add Ruby solution for 2023 day 5 part 2

--- src/2023/05/solve.rb
+++ src/2023/05/solve.rb
@@ -8,6 +8,7 @@ class Map
     @source = source
     @dest = dest
     @overrides = []
+    @rev_overrides = []
   end
 
   def add(dest_start, source_start, length)
@@ -17,10 +18,25 @@ class Map
       dest_start + offset
     }
     @overrides << [checker, converter]
+
+    rev_checker = -> (d_id) { d_id >= dest_start && d_id <= dest_start + length - 1 }
+    rev_converter = -> (d_id) {
+      offset = d_id - dest_start
+      source_start + offset
+    }
+    @rev_overrides << [rev_checker, rev_converter]
   end
 
   def resolve(id)
     @overrides.each do |checker, converter|
+      return converter.(id) if checker.(id)
+    end
+
+    id
+  end
+
+  def rev_resolve(id)
+    @rev_overrides.each do |checker, converter|
       return converter.(id) if checker.(id)
     end
 
@@ -34,6 +50,7 @@ end
 
 seeds = []
 maps = {}
+rev_maps = {}
 
 ARGF.read.lines(chomp: true).each do |line|
   case line
@@ -41,7 +58,9 @@ ARGF.read.lines(chomp: true).each do |line|
     seeds = line.scan(/\d+/).map(&:to_i)
   when /^(\w+)-to-(\w+) map/
     $source = $1.to_sym
-    maps[$source] = Map.new $1.to_sym, $2.to_sym
+    $dest = $2.to_sym
+    maps[$source] = Map.new $source, $dest
+    rev_maps[$dest] = maps[$source]
   when /^(\d+) (\d+) (\d+)/
     maps[$source].add $1.to_i, $2.to_i, $3.to_i
   end
@@ -62,3 +81,19 @@ end
 
 puts lowest.min
 
+ranges = seeds.each_slice(2).map { (_1..(_1 + _2 - 1)) }
+
+(0..).each do |final|
+  current = rev_maps[:location]
+  value = final
+  while current.source != :seed
+    new_val = current.rev_resolve(value)
+    value = new_val
+    current = rev_maps[current.source]
+  end
+  value = maps[:seed].rev_resolve(value)
+  if ranges.any? { |r| r.include?(value) }
+    puts final
+    exit
+  end
+end
