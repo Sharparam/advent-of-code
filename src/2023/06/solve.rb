4d7bdc05797747ffec490df9bc5b15cc0cd9af7a
Adam Hellberg <sharparam@sharparam.com>
2023-12-06 07:40:09 +0100

    Shave off a byte

--- src/2023/06/solve.rb
+++ src/2023/06/solve.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-times, distances = ARGF.read.lines.map { _1.scan(/\d+/).map(&:to_i) }
+    times, distances = ARGF.readlines.map { _1.scan(/\d+/).map(&:to_i) }
 
-puts times.zip(distances).reduce(1) { |total, (time, distance)|
-  total * (0..time).count { (time - _1) * _1 > distance }
-}
+    puts times.zip(distances).reduce(1) { |total, (time, distance)|
+      total * (0..time).count { (time - _1) * _1 > distance }
+    }
 
-time = times.join.to_i
-distance = distances.join.to_i
+    time = times.join.to_i
+    distance = distances.join.to_i
 
-puts (0..time).count { (time - _1) * _1 > distance }
+    puts (0..time).count { (time - _1) * _1 > distance }
