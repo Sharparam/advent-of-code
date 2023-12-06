ac7b2dbadd96def72008c85cff3f3dcb3549ab2c
Adam Hellberg <sharparam@sharparam.com>
2023-12-06 06:22:45 +0100

    Reduce code

--- src/2023/06/solve.rb
+++ src/2023/06/solve.rb
@@ -4,10 +4,10 @@
 times, distances = ARGF.read.lines.map { _1.scan(/\d+/).map(&:to_i) }
 
 puts times.zip(distances).reduce(1) { |total, (time, distance)|
-  total * (0..time).count { |t| (time - t) * t > distance }
+  total * (0..time).count { (time - _1) * _1 > distance }
 }
 
 time = times.join.to_i
 distance = distances.join.to_i
 
-puts (0..time).count { |t| (time - t) * t > distance }
+puts (0..time).count { (time - _1) * _1 > distance }
