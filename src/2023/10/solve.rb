b8415fe51bc01e912cbb4721c71bc8cf75a1c7ff
Adam Hellberg <sharparam@sharparam.com>
2023-12-10 14:12:45 +0100

    Improve 2023 day 10

--- src/2023/10/solve.rb
+++ src/2023/10/solve.rb
@@ -11,11 +11,19 @@ FANCIES = {
   J: '┘',
   '7': '┐',
   F: '┌',
-  '.': '·',
-  dot: '·',
-  wall: '█',
-  empty: ' '
+  '.': '·'
 }
+
+def display(grid)
+  height = grid.keys.map { _1[1] }.max + 1
+  width = grid.keys.map { _1[0] }.max + 1
+  (0...height).each do |row|
+    (0...width).each do |col|
+      print FANCIES[grid[Vector[col, row]]]
+    end
+    puts
+  end
+end
 
 grid = {}
 start = nil
@@ -30,19 +38,7 @@ end
 WIDTH = grid.keys.map { _1[0] }.max + 1
 HEIGHT = grid.keys.map { _1[1] }.max + 1
 
-DIRS = {
-  C: Vector[0, 0],
-  N: Vector[0, -1],
-  S: Vector[0, 1],
-  W: Vector[-1, 0],
-  E: Vector[1, 0],
-  NE: Vector[1, -1],
-  NW: Vector[-1, -1],
-  SE: Vector[1, 1],
-  SW: Vector[-1, 1]
-}
-
-neighbours = {
+NEIGHBORS = {
   '|': [Vector[0, -1], Vector[0, 1]],
   '-': [Vector[-1, 0], Vector[1, 0]],
   L: [Vector[0, -1], Vector[1, 0]],
@@ -51,17 +47,7 @@ neighbours = {
   F: [Vector[1, 0], Vector[0, 1]]
 }
 
-large_tiles = {
-  '|': { type: :wall, fill: [DIRS[:N], Vector[0, 0], DIRS[:S]] },
-  '-': { type: :wall, fill: [DIRS[:W], Vector[0, 0], DIRS[:E]] },
-  L: { type: :wall, fill: [DIRS[:N], Vector[0, 0], DIRS[:E]] },
-  J: { type: :wall, fill: [DIRS[:N], Vector[0, 0], DIRS[:W]] },
-  '7': { type: :wall, fill: [DIRS[:W], Vector[0, 0], DIRS[:S]] },
-  F: { type: :wall, fill: [DIRS[:E], Vector[0, 0], DIRS[:S]] },
-  '.': { type: :dot, fill: [Vector[0, 0]] }
-}
-
-neighbours.keys.each do |type|
+NEIGHBORS.keys.each do |type|
   case type
   when :'|'
     next if start[1] == 0 || start[1] == HEIGHT - 1
@@ -92,21 +78,17 @@ neighbours.keys.each do |type|
   break
 end
 
-dists = Hash.new(Float::INFINITY)
+dists = Hash.new(Float::INFINITY).tap { _1[start] = 0 }
+STARTS = NEIGHBORS[grid[start]]
 
-dists[start] = 0
-
-
-dirs = neighbours[grid[start]]
-
-dirs.each do |start_dir|
+STARTS.each do |start_dir|
   seen = Set.new [start]
   current = start + start_dir
   cost = 1
   while !current.nil? && current != start
     seen.add current
     dists[current] = cost if cost < dists[current]
-    cands = neighbours[grid[current]].map { current + _1 }
+    cands = NEIGHBORS[grid[current]].map { current + _1 }
     current = cands.reject { !grid.include?(_1) || seen.include?(_1) }[0]
     cost += 1
   end
@@ -114,66 +96,21 @@ end
 
 puts dists.values.max
 
-loop_tiles = dists.keys
+(grid.keys - dists.keys).each { grid[_1] = :'.' }
 
-(grid.keys - loop_tiles).each { grid[_1] = :'.' }
+FLIPS = Set.new %i[| F 7]
+inside_count = 0
 
-def display(grid)
-  height = grid.keys.map { _1[1] }.max + 1
-  width = grid.keys.map { _1[0] }.max + 1
-  (0...height).each do |row|
-    (0...width).each do |col|
-      print FANCIES[grid[Vector[col, row]]]
+(0...HEIGHT).each do |y|
+  state = false
+  (0...WIDTH).each do |x|
+    type = grid[Vector[x, y]]
+    if FLIPS.include?(type)
+      state = !state
+    elsif type == :'.' && state
+      inside_count += 1
     end
-    puts
   end
 end
-
-large_grid = {}
-
-grid.each do |pos, tile|
-  pos = Vector[pos[0] * 3, pos[1] * 3]
-  large_tile = large_tiles[tile]
-  type = large_tile[:type]
-  fill = large_tile[:fill]
-  DIRS.values.each {
-    new = pos + _1
-    t = fill.include?(_1) ? type : :empty
-    if large_grid[new].nil? || (large_grid[new] == :empty && t != :empty)
-      large_grid[new] = t
-    end
-  }
-end
-
-# display grid
-# display large_grid
-
-NEXT = [DIRS[:N], DIRS[:S], DIRS[:W], DIRS[:E]]
-
-width = large_grid.keys.map { _1[0] }.max + 1
-height = large_grid.keys.map { _1[1] }.max + 1
-
-outside = Set.new
-visited = Set.new
-
-queue = large_grid.select { |p, t| t != :wall && (p[0] == 0 || p[0] == width - 1 || p[1] == 0 || p[1] == height - 1) }.map { |p, _| p }
-
-while queue.any?
-  current = queue.shift
-  next if visited.include? current
-  visited.add current
-  outside.add current if large_grid[current] == :dot
-  around = NEXT.map { current + _1 }.reject {
-    visited.include?(_1) || queue.include?(_1) ||
-    _1[0] < 0 || _1[0] > width - 2 ||
-    _1[1] < 0 || _1[1] > height - 2 ||
-    large_grid[_1] == :wall
-  }
-  queue.concat around
-end
-
-outside_count = outside.size
-total_count = large_grid.count { |_, t| t == :dot }
-inside_count = total_count - outside_count
 
 puts inside_count
