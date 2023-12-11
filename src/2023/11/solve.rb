28bb60205a747ef44757f08e7dc2153d46ac24c6
Adam Hellberg <sharparam@sharparam.com>
2023-12-11 07:04:07 +0100

    Add Ruby solution for 2023 day 11

---
+++ src/2023/11/solve.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'set'
+
+POSITIONS = ARGF.readlines(chomp: true).flat_map.with_index { |line, y|
+  line.chars.map.with_index { [_1, _2] }.select { |c, _| c == ?# }.map { [_2, y] }
+}
+
+XS, YS = POSITIONS.map { [_1[0], _1[1]] }.transpose.map { _1.to_set }
+
+WIDTH = XS.max + 1
+HEIGHT = YS.max + 1
+
+EMPTY_X = (0...WIDTH).select { !XS.include?(_1) }.to_set
+EMPTY_Y = (0...HEIGHT).select { !YS.include?(_1) }.to_set
+
+def dist(a, b)
+  x1, x2, y1, y2 = a[0], b[0], a[1], b[1]
+  x_min, x_max = [x1, x2].minmax
+  y_min, y_max = [y1, y2].minmax
+  ex = (x_min..x_max).count { EMPTY_X.include? _1 }
+  ey = (y_min..y_max).count { EMPTY_Y.include? _1 }
+  dist = (x1 - x2).abs + (y1 - y2).abs
+  [dist + ex + ey, dist + ex * 999_999 + ey * 999_999]
+end
+
+puts POSITIONS.combination(2).to_a.map { dist _1, _2 }.transpose.map(&:sum)
