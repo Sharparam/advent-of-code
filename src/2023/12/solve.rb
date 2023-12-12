540e3cf9521b62b2e660c817ec85e822710393e7
Adam Hellberg <sharparam@sharparam.com>
2023-12-12 07:01:59 +0100

    Add Ruby solution for 2023 day 12 part 1

---
+++ src/2023/12/solve.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+def gen(chars, i = 0, result = [''])
+  return result if i == chars.size
+
+  c = chars[i]
+
+  if c != ??
+    return gen chars, i + 1, result.map { _1 + c }
+  end
+
+  new = result.flat_map { [_1 + ?., _1 + ?#] }
+  gen chars, i + 1, new
+end
+
+puts ARGF.readlines(chomp: true).sum { |line|
+  records, counts = line.split ' '
+  counts = counts.split(',').map(&:to_i)
+  m = counts.map { "\#{#{_1}}" }.join "\\.+"
+  re = Regexp.new "^\\.*#{m}\\.*$"
+
+  gen(records).count { re.match? _1 }
+}
