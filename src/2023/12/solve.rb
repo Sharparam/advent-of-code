f47c1015835a56f695827a7e294208bd67e4bc6c
Adam Hellberg <sharparam@sharparam.com>
2023-12-12 18:18:27 +0100

    Improve 2023 day 12 a bit

--- src/2023/12/solve.rb
+++ src/2023/12/solve.rb
@@ -1,33 +1,32 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-class NilClass; def empty?; true; end; end
+class NilClass; def empty?; true; end; def size; 0; end; end
 
-CACHE = {}
-
-def valids(records, counts)
-  key = [records, counts].hash
-  return CACHE[key] if CACHE.key? key
+def valids(records, counts, row_size, cache = {}, skip_cache = false)
+  key = records.size + counts.size * row_size
+  return cache[key] if cache.key?(key) && !skip_cache
   return 1 if records.empty? && !counts.any?
   return 0 if records.empty? && counts.any?
   return 0 if records[0] == ?# && !counts.any?
-  return CACHE[key] = valids(records[1..], counts) if records[0] == ?.
+  return cache[key] = valids(records[1..], counts, row_size, cache) if records[0] == ?.
   if records[0] == ??
     rest = records[1..]
-    return CACHE[key] = valids(rest, counts) + valids("##{rest}", counts)
+    return cache[key] = valids(rest, counts, row_size, cache) + valids("##{rest}", counts, row_size, cache, true)
   end
   return 0 if records.size < counts[0]
   return 0 if records[..counts[0] - 1].include? ?.
   return 0 if records.size >= counts[0] && records[counts[0]] == ?#
 
-  CACHE[key] = valids(records[counts[0] + 1..], counts[1..])
+  cache[key] = valids(records[counts[0] + 1..], counts[1..], row_size, cache)
 end
 
 puts ARGF.readlines.each_with_index.reduce([0, 0]) { |a, (line, i)|
   records, counts = line.split ' '
   counts = counts.split(',').map(&:to_i)
-  p1 = valids records, counts
-  p2 = valids ([records] * 5).join(??), counts * 5
+  records_2, counts_2 = ([records] * 5).join(??), counts * 5
+  p1 = valids records, counts, records.size
+  p2 = valids records_2, counts_2, records_2.size
 
   [a[0] + p1, a[1] + p2]
 }
