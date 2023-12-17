067c0488543365c327c3876bf0816251ad277000
Adam Hellberg <sharparam@sharparam.com>
2023-12-17 11:31:11 +0100

    Improve Ruby solution for 2023 day 17

--- src/2023/17/solve.rb
+++ src/2023/17/solve.rb
@@ -4,21 +4,51 @@
 require 'matrix'
 require 'set'
 
-require_relative '../../../lib/pqueue'
+# using an external gem for priority queue
+# I could use my own (in /lib/pqueue.rb), but it's quite a bit slower
+require 'algorithms' # gem install algorithms
+
+def cantor(x, y)
+  ((x + y) * (x + y + 1)) / 2 + y
+end
+
+def cantor3(x, y, z)
+  cantor(cantor(x, y), z)
+end
+
+# Using our own Point class instead of Vector speeds the code up by several
+# seconds. Probably because this Point class is specialized for 2 fields only
+# (x and y) whereas the Vector class is generalized to handle any number
+# of fields.
+class Point
+  attr_reader :x, :y, :hash
+
+  private def initialize(x, y)
+    @x = x
+    @y = y
+    @hash = cantor(x, y)
+  end
+
+  def self.[](x, y) = Point.new x, y
+  def +(other) = Point.new x + other.x, y + other.y
+  def *(n) = Point.new x * n, y * n
+  def ==(other) = hash == other.hash
+  def eql?(other) = hash.eql? other.hash
+end
 
 GRID = Hash[ARGF.readlines(chomp: true).flat_map.with_index { |line, y|
-  line.chars.map.with_index { |c, x| [Vector[x, y], c.to_i] }
+  line.chars.map.with_index { |c, x| [Point[x, y], c.to_i] }
 }].tap { _1.default = Float::INFINITY }
 
-WIDTH = GRID.keys.map { _1[0] }.max + 1
-HEIGHT = GRID.keys.map { _1[1] }.max + 1
+WIDTH = GRID.keys.map(&:x).max + 1
+HEIGHT = GRID.keys.map(&:y).max + 1
 
-START = Vector[0, 0]
-GOAL = Vector[WIDTH - 1, HEIGHT - 1]
+START = Point[0, 0]
+GOAL = Point[WIDTH - 1, HEIGHT - 1]
 
-DIRS = %i[R L D U]
-OPPOSITES = { R: :L, L: :R, D: :U, U: :D }
-DIR_TO_V = { R: Vector[1, 0], L: Vector[-1, 0], D: Vector[0, 1], U: Vector[0, -1] }
+DIRS = [0, 1, 2, 3]
+OPPOSITES = [1, 0, 3, 2]
+DIR_TO_V = [Point[1, 0], Point[-1, 0], Point[0, 1], Point[0, -1]]
 
 def neighbors(current, last_dir, min, max)
   valid_dirs = DIRS - [last_dir, OPPOSITES[last_dir]]
@@ -29,20 +59,21 @@ def neighbors(current, last_dir, min, max)
       m.times { |i| cost += GRID[current + v * (i + 1)] }
       [current + v * m, dir, cost]
     }
-  }.reject { |p, _, _| p[0] < 0 || p[0] >= WIDTH || p[1] < 0 || p[1] >= HEIGHT }
+  }.reject { |p, _, _| p.x < 0 || p.x >= WIDTH || p.y < 0 || p.y >= HEIGHT }
 end
 
 def solve(source = START, target = GOAL, &neighbors)
-  queue = PriorityQueue.new
-  queue.enqueue [source, nil, 0], 0
+  queue = Containers::PriorityQueue.new { |a, b| (a <=> b) == -1 }
+  queue.push [source, -1, 0], 0
+
   seen = Set.new
 
-  while queue.any?
-    current, last_dir, cost = queue.dequeue
-    next unless seen.add? [current, last_dir]
+  while !queue.empty?
+    current, last_dir, cost = queue.pop
+    next unless seen.add? cantor(current.hash, last_dir)
     return cost if current == target
     neighbors.call(current, last_dir).each do |np, d, c|
-      queue.enqueue [np, d, cost + c], cost + c
+      queue.push [np, d, cost + c], cost + c
     end
   end
 end
