5335ee5a8309acb7100ccafdb26da876a9517fd9
Adam Hellberg <sharparam@sharparam.com>
2023-12-22 14:13:28 +0100

    Improve 2023 day 22

--- src/2023/22/solve.rb
+++ src/2023/22/solve.rb
@@ -1,43 +1,15 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'set'
-
-def cantor(x, y)
-  ((x + y) * (x + y + 1)) / 2 + y
-end
-
-def cantor_n(*v)
-  v[1..].reduce(v[0]) { |a, e| cantor(a, e) }
-end
-
-class Point
-  attr_reader :x, :y
-
-  def initialize(x, y)
-    @x, @y = x, y
-  end
-
-  def self.[](x, y) = Point.new x, y
-
-  def ==(other) = @hash == other.hash
-  def eql?(other) = self == other
-  def dup = Point.new x, y
-  def clone = dup
-  def to_s = "(#{x}, #{y})"
-  def inspect = to_s
-end
-
 class Rect
-  attr_reader :start, :stop, :hash
+  attr_reader :startx, :starty, :stopx, :stopy
 
-  def initialize(start, stop)
-    @start, @stop = start, stop
-    @hash = cantor(start.hash, stop.hash)
+  def initialize(startx, starty, stopx, stopy)
+    @startx, @starty, @stopx, @stopy = startx, starty, stopx, stopy
   end
 
   def intersects?(other)
-    return false if stop.x < other.start.x || start.x > other.stop.x || stop.y < other.start.y || start.y > other.stop.y
+    return false if stopx < other.startx || startx > other.stopx || stopy < other.starty || starty > other.stopy
     true
   end
 end
@@ -49,19 +21,10 @@ class Brick
 
   def initialize(startx, starty, startz, stopx, stopy, stopz)
     @startx, @starty, @startz, @stopx, @stopy, @stopz = startx, starty, startz, stopx, stopy, stopz
-    @rect = Rect.new Point[@startx, @starty], Point[@stopx, @stopy]
-  end
-
-  def rests_on?(other, offset = 0)
-    return false unless @startz - offset == other.stopz + 1
-    return rect.intersects? other.rect
+    @rect = Rect.new @startx, @starty, @stopx, @stopy
   end
 
   def dup = Brick.new @startx, @starty, @startz, @stopx, @stopy, @stopz
-  def clone = dup
-
-  def to_s = "(#{@startx}, #{@starty}, #{@startz}) ~ (#{@stopx}, #{@stopy}, #{@stopz})"
-  def inspect = to_s
 end
 
 class Grid
@@ -69,102 +32,32 @@ class Grid
 
   def initialize(bricks = [])
     @bricks = bricks
-    @hash = @bricks.hash
-  end
-
-  def push(brick)
-    @bricks.push brick
-  end
-
-  def <<(brick)
-    push brick
-  end
-
-  def concat(bricks)
-    @bricks.concat bricks
   end
 
   def dup = Grid.new @bricks.map(&:dup)
-  def clone = dup
 
   def settle!
     sorted_bricks = @bricks.select { |b| b.startz > 1 }.sort_by { |b| b.startz }
 
-    below = Hash.new { |h, k| h[k] = [] }
-
-    @bricks.select { |b| b.startz == 1 }.each { |b| below[b.stopz].push b }
-
+    rects = Hash.new { |h, k| h[k] = [] }
+    @bricks.select { |b| b.startz == 1 }.each { |b| rects[b.stopz].push b.rect }
     moved = 0
 
     sorted_bricks.each do |brick|
-      next below[brick.stopz].push brick if below[brick.startz - 1].any? { |b| brick.rests_on? b }
+      next rects[brick.stopz].push brick.rect if rects[brick.startz - 1].any? { |r| brick.rect.intersects? r }
       moved += 1
       reduction = 1
-      reduction += 1 while brick.startz - reduction > 1 && !below[brick.startz - 1 - reduction].any? { |b| brick.rests_on?(b, reduction) }
+      reduction += 1 while brick.startz - reduction > 1 && !rects[brick.startz - 1 - reduction].any? { |r| brick.rect.intersects? r }
       brick.startz -= reduction
       brick.stopz -= reduction
-      below[brick.stopz].push brick
+      rects[brick.stopz].push brick.rect
     end
 
     moved
   end
-
-  def display
-    x_min = @bricks.map { |b| b.startx }.min
-    x_max = @bricks.map { |b| b.stopx }.max
-    y_min = @bricks.map { |b| b.starty }.min
-    y_max = @bricks.map { |b| b.stopy }.max
-    z_min = 0
-    z_max = @bricks.map { |b| b.stopz }.max
-    x_width = x_max - x_min + 1
-    y_width = y_max - y_min + 1
-    x_floor = "-" * x_width
-    y_floor = "-" * y_width
-
-    puts " x#{' ' * (x_width - 2)}   y#{' ' * (y_width - 2)}"
-    x_width.times { |i| print i }
-    print "  "
-    y_width.times { |i| print i }
-    puts
-    z_max.downto(0).each do |z|
-      if z == 0
-        puts "#{x_floor}  #{y_floor}  0"
-        next
-      end
-
-      x_width.times do |x|
-        if @bricks.any? { |b| (b.startx .. b.stopx).include?(x) && (b.startz .. b.stopz).include?(z) }
-          print '#'
-        else
-          print '.'
-        end
-      end
-
-      print "  "
-
-      y_width.times do |y|
-        if @bricks.any? { |b| (b.starty .. b.stopy).include?(y) && (b.startz .. b.stopz).include?(z) }
-          print '#'
-        else
-          print '.'
-        end
-      end
-
-      puts "  #{z}"
-    end
-  end
 end
 
-grid = Grid.new ARGF.readlines(chomp: true).map {
-  _1.split ?~
-}.map { |first, second|
-  starts = first.split(?,).map(&:to_i)
-  stops = second.split(?,).map(&:to_i)
-  startx, stopx = [starts[0], stops[0]].minmax
-  starty, stopy = [starts[1], stops[1]].minmax
-  startz, stopz = [starts[2], stops[2]].minmax
-  Brick.new startx, starty, startz, stopx, stopy, stopz
-}
+grid = Grid.new ARGF.readlines.map { Brick.new *_1.scan(/\d+/).map(&:to_i) }
 
 grid.settle!
 
