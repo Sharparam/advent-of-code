6b626d512ce884190f5756b6a060ab082e6aa5a2
Adam Hellberg <sharparam@sharparam.com>
2023-12-22 13:59:22 +0100

    Solve 2023 day 22

--- src/2023/22/solve.rb
+++ src/2023/22/solve.rb
@@ -1,78 +1,66 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'matrix'
 require 'set'
 
-class Range
-  def intersects?(other)
-    (self.max >= other.min && self.max <= other.max) || (self.min >= other.min && self.min <= other.max)
-  end
+def cantor(x, y)
+  ((x + y) * (x + y + 1)) / 2 + y
 end
 
-def make_range(a, b)
-  min, max = [a, b].minmax
-  (min..max)
+def cantor_n(*v)
+  v[1..].reduce(v[0]) { |a, e| cantor(a, e) }
 end
 
 class Point
-  include Comparable
-  attr_reader :x, :y, :z
+  attr_reader :x, :y
 
-  def initialize(x, y, z)
-    @x, @y, @z = x, y, z
-    @hash = [@x, @y, @z].hash
+  def initialize(x, y)
+    @x, @y = x, y
   end
 
-  def self.[](x, y, z)
-    Point.new x, y, z
-  end
+  def self.[](x, y) = Point.new x, y
 
-  def <=>(other)
-    return z <=> other.z unless z == other.z
-    return x <=> other.x unless x == other.x
-    return y <=> other.y unless y == other.y
-    0
-  end
-
+  def ==(other) = @hash == other.hash
   def eql?(other) = self == other
-  def hash = @hash
-
-  def dup = Point[@x, @y, @z]
+  def dup = Point.new x, y
   def clone = dup
-
-  def to_s = "(#{@x}, #{@y}, #{@z})"
+  def to_s = "(#{x}, #{y})"
   def inspect = to_s
-  def to_v = Vector[@x, @y, @z]
 end
 
-class Brick
-  attr_reader :start, :stop
-  attr_reader :xrange, :yrange, :zrange
+class Rect
+  attr_reader :start, :stop, :hash
 
   def initialize(start, stop)
     @start, @stop = start, stop
-    raise "start x > stop x" if start.x > stop.x
-    raise "start y > stop y" if start.y > stop.y
-    raise "start y > stop y" if start.y > stop.y
-    @xrange = (@start.x .. @stop.x) # make_range @start.x, @stop.x
-    @yrange = (@start.y .. @stop.y) # make_range @start.y, @stop.y
-    @zrange = (@start.z .. @stop.z) # make_range @start.z, @stop.z
-    @hash = [start, stop].hash
+    @hash = cantor(start.hash, stop.hash)
   end
 
-  def ==(other) = @start == other.start && @stop == other.stop
-  def eql?(other) = self == other
-  def hash = @hash
+  def intersects?(other)
+    return false if stop.x < other.start.x || start.x > other.stop.x || stop.y < other.start.y || start.y > other.stop.y
+    true
+  end
+end
 
-  def include?(point)
-    @xrange.include?(point.x) && @yrange.include?(point.y) && @zrange.include?(point.z)
+class Brick
+  attr_reader :startx, :starty, :stopx, :stopy
+  attr_accessor :startz, :stopz
+  attr_reader :rect
+
+  def initialize(startx, starty, startz, stopx, stopy, stopz)
+    @startx, @starty, @startz, @stopx, @stopy, @stopz = startx, starty, startz, stopx, stopy, stopz
+    @rect = Rect.new Point[@startx, @starty], Point[@stopx, @stopy]
   end
 
-  def dup = Brick.new @start.dup, @stop.dup
+  def rests_on?(other, offset = 0)
+    return false unless @startz - offset == other.stopz + 1
+    return rect.intersects? other.rect
+  end
+
+  def dup = Brick.new @startx, @starty, @startz, @stopx, @stopy, @stopz
   def clone = dup
 
-  def to_s = "#{start} ~ #{stop}"
+  def to_s = "(#{@startx}, #{@starty}, #{@startz}) ~ (#{@stopx}, #{@stopy}, #{@stopz})"
   def inspect = to_s
 end
 
@@ -99,96 +87,35 @@ class Grid
   def dup = Grid.new @bricks.map(&:dup)
   def clone = dup
 
-  def step
-    # puts "stepping"
-    z_max = @bricks.map { |b| b.zrange.max }.max
+  def settle!
+    sorted_bricks = @bricks.select { |b| b.startz > 1 }.sort_by { |b| b.startz }
 
-    return [self.dup, false] if z_max < 2
+    below = Hash.new { |h, k| h[k] = [] }
 
-    new_grid = Grid.new
-    moved = false
+    @bricks.select { |b| b.startz == 1 }.each { |b| below[b.stopz].push b }
 
-    (1..z_max).each do |z|
-      z_bricks = @bricks.select { |b| b.zrange.min == z }
-      if z == 1
-        new_grid.concat z_bricks.map(&:dup)
-        next
-      end
-      below_bricks = @bricks.select { |b| b.zrange.max == z - 1 }
-      # xranges, yranges = below_bricks.map { |b| [b.xrange, b.yrange] }.transpose
-      xs = below_bricks.flat_map { |b| b.xrange.to_a }.uniq
-      ys = below_bricks.flat_map { |b| b.yrange.to_a }.uniq
-      xys = xs.product(ys).to_set
-      # puts "at Z = #{z}, supporting coords: #{xys}"
-      # puts "#{bricks.size} bricks to process"
-      z_bricks.each do |brick|
-        bxs = brick.xrange.to_a
-        bys = brick.yrange.to_a
-        bxys = bxs.product(bys)
-        supported = bxys.any? { xys.include? _1 }
-        if supported
-          new_grid.push brick.dup
-        else
-          new_brick = Brick.new(Point[brick.start.x, brick.start.y, brick.start.z - 1], Point[brick.stop.x, brick.stop.y, brick.stop.z - 1])
-          # puts "  #{brick} is moving to #{new_brick}"
-          new_grid.push new_brick
-          moved = true
-        end
-      end
+    moved = 0
+
+    sorted_bricks.each do |brick|
+      next below[brick.stopz].push brick if below[brick.startz - 1].any? { |b| brick.rests_on? b }
+      moved += 1
+      reduction = 1
+      reduction += 1 while brick.startz - reduction > 1 && !below[brick.startz - 1 - reduction].any? { |b| brick.rests_on?(b, reduction) }
+      brick.startz -= reduction
+      brick.stopz -= reduction
+      below[brick.stopz].push brick
     end
 
-    [new_grid, moved]
-  end
-
-  def is_settled?
-    z_max = @bricks.map { |b| b.zrange.max }.max
-
-    return true if z_max < 2
-
-    settled = true
-
-    (1..z_max).each do |z|
-      z_bricks = @bricks.select { |b| b.zrange.min == z }
-      if z == 1
-        next
-      end
-      below_bricks = @bricks.select { |b| b.zrange.max == z - 1 }
-      xs = below_bricks.flat_map { |b| b.xrange.to_a }.uniq
-      ys = below_bricks.flat_map { |b| b.yrange.to_a }.uniq
-      xys = xs.product(ys).to_set
-      z_bricks.each do |brick|
-        bxs = brick.xrange.to_a
-        bys = brick.yrange.to_a
-        bxys = bxs.product(bys)
-        supported = bxys.any? { xys.include? _1 }
-        if supported
-          next
-        else
-          settled = false
-        end
-      end
-    end
-
-    settled
-  end
-
-  def settle
-    new_grid, moved = step
-    while moved
-      # puts "continuing"
-      new_grid, moved = new_grid.step
-    end
-
-    new_grid
+    moved
   end
 
   def display
-    x_min = @bricks.map { |b| b.xrange.min }.min
-    x_max = @bricks.map { |b| b.xrange.max }.max
-    y_min = @bricks.map { |b| b.yrange.min }.min
-    y_max = @bricks.map { |b| b.yrange.max }.max
+    x_min = @bricks.map { |b| b.startx }.min
+    x_max = @bricks.map { |b| b.stopx }.max
+    y_min = @bricks.map { |b| b.starty }.min
+    y_max = @bricks.map { |b| b.stopy }.max
     z_min = 0
-    z_max = @bricks.map { |b| b.zrange.max }.max
+    z_max = @bricks.map { |b| b.stopz }.max
     x_width = x_max - x_min + 1
     y_width = y_max - y_min + 1
     x_floor = "-" * x_width
@@ -206,7 +133,7 @@ class Grid
       end
 
       x_width.times do |x|
-        if @bricks.any? { |b| b.xrange.include?(x) && b.zrange.include?(z) }
+        if @bricks.any? { |b| (b.startx .. b.stopx).include?(x) && (b.startz .. b.stopz).include?(z) }
           print '#'
         else
           print '.'
@@ -216,7 +143,7 @@ class Grid
       print "  "
 
       y_width.times do |y|
-        if @bricks.any? { |b| b.yrange.include?(y) && b.zrange.include?(z) }
+        if @bricks.any? { |b| (b.starty .. b.stopy).include?(y) && (b.startz .. b.stopz).include?(z) }
           print '#'
         else
           print '.'
@@ -236,29 +163,20 @@ grid = Grid.new ARGF.readlines(chomp: true).map {
   startx, stopx = [starts[0], stops[0]].minmax
   starty, stopy = [starts[1], stops[1]].minmax
   startz, stopz = [starts[2], stops[2]].minmax
-  start = Point[startx, starty, startz]
-  stop = Point[stopx, stopy, stopz]
-  Brick.new start, stop
+  Brick.new startx, starty, startz, stopx, stopy, stopz
 }
 
-settled = grid.settle
-
-puts "settled"
-
-settled.display
-
-bricks = settled.bricks.map(&:dup)
-
-puts "#{bricks.size} bricks to test"
+grid.settle!
 
 n = 0
-bricks.size.times do |i|
-  # puts "testing index #{i}"
-  new_bricks = bricks.dup
-  new_bricks.delete_at i
-  new_grid = Grid.new new_bricks
-  settled = new_grid.is_settled?
-  n += 1 if settled
+total = 0
+grid.bricks.size.times do |i|
+  new_grid = grid.dup
+  new_grid.bricks.delete_at i
+  moved = new_grid.settle!
+  n += 1 if moved == 0
+  total += moved
 end
 
 puts n
+puts total
