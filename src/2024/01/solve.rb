acad2457ea3c73ddf7728ff657368915ef921568
Adam Hellberg <sharparam@sharparam.com>
2024-12-01 08:42:24 +0100

    Improve Ruby solution

--- src/2024/01/solve.rb
+++ src/2024/01/solve.rb
@@ -2,5 +2,5 @@
 # frozen_string_literal: true
 
 left, right = ARGF.readlines.map(&:split).map { _1.map(&:to_i) }.transpose
-puts left.sort.zip(right.sort).map { (_1 - _2).abs }.sum
+puts left.sort.zip(right.sort).sum { (_1 - _2).abs }
 puts right.tally.then { |c| left.sum { _1 * (c[_1] || 0) } }
