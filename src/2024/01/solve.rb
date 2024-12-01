3372587d22cee1099541a4f0d7da35c295d2b9aa
Adam Hellberg <sharparam@sharparam.com>
2024-12-01 06:45:34 +0100

    Improve 2024 day 1

--- src/2024/01/solve.rb
+++ src/2024/01/solve.rb
@@ -3,4 +3,4 @@
 
 left, right = ARGF.readlines.map(&:split).map { _1.map(&:to_i) }.transpose
 puts left.sort.zip(right.sort).map { (_1 - _2).abs }.sum
-puts left.map { [_1, right.count(_1)] }.map { _1 * _2 }.sum
+puts right.tally.then { |c| left.sum { _1 * (c[_1] || 0) } }
