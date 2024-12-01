f531d161509af0555096517bd57905ffa730380a
Adam Hellberg <sharparam@sharparam.com>
2024-12-02 00:32:49 +0100

    Improve Ruby solution for 2024 day 1

--- src/2024/01/solve.rb
+++ src/2024/01/solve.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-left, right = ARGF.readlines.map(&:split).map { _1.map(&:to_i) }.transpose
+left, right = ARGF.map { _1.split.map(&:to_i) }.transpose
 puts left.sort.zip(right.sort).sum { (_1 - _2).abs }
 puts right.tally.then { |c| left.sum { _1 * (c[_1] || 0) } }
