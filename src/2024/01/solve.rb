fe38feaefc1ec6aac8db4fcb4f70c8d36bbce039
Adam Hellberg <sharparam@sharparam.com>
2024-12-01 06:21:01 +0100

    Add solution for 2024 day 1

---
+++ src/2024/01/solve.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+left, right = ARGF.readlines.map(&:split).map { _1.map(&:to_i) }.transpose
+puts left.sort.zip(right.sort).map { (_1 - _2).abs }.sum
+puts left.map { [_1, right.count(_1)] }.map { _1 * _2 }.sum
