a3c686b24b02aea4c349cbc762049569269da178
Adam Hellberg <sharparam@sharparam.com>
2024-12-03 06:24:40 +0100

    Add Ruby solution for 2024 day 3

---
+++ src/2024/03/solve.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+class Computer
+  def initialize(advanced = false)
+    @advanced = advanced
+    @enabled = true
+  end
+
+  def MUL(x, y)
+    @enabled ? x * y : 0
+  end
+
+  def DO()
+    return 0 unless @advanced
+    @enabled = true
+    0
+  end
+
+  def DONT()
+    return 0 unless @advanced
+    @enabled = false
+    0
+  end
+end
+
+instrs = ARGF.read.upcase.gsub("DON'T", "DONT").scan(/MUL\(\d+,\d+\)|DO(?:NT)?\(\)/)
+
+puts Computer.new.then { |c| instrs.map { c.instance_eval _1 }.sum }
+puts Computer.new(true).then { |c| instrs.map { c.instance_eval _1 }.sum }
