2028b97f8466d9bbf95f4037086718c4bbc48454
Adam Hellberg <sharparam@sharparam.com>
2024-12-05 06:40:57 +0100

    Add Ruby solution for 2024 day 5

---
+++ src/2024/05/solve.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+orderings, updates = ARGF.read.split "\n\n"
+
+orderings = orderings.lines.map { _1.split('|').map(&:to_i) }
+RULES = orderings.reduce({}) { |a, (left, right)| (a[left] ||= Set.new).add(right); a }
+updates = updates.lines.map { _1.split(",").map(&:to_i) }
+
+incorrect = []
+
+puts updates.sum { |pages|
+  if pages.size.times.all? { |i|
+      *before, current = pages[..i]
+      !RULES.key?(current) || before.all? { |t| !RULES[current].include? t }
+    }
+    next pages[pages.size / 2]
+  end
+  incorrect << pages
+  0
+}
+
+def sort(a, b)
+  if RULES.key?(a) && RULES[a].include?(b)
+    -1
+  elsif RULES.key?(b) && RULES[b].include?(a)
+    1
+  else
+    0
+  end
+end
+
+# p middles.sum
+
+p incorrect.map { _1.sort(&method(:sort)) }.sum { _1[_1.size / 2] }
