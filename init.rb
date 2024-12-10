faf825bed8cc081d6a6f68a90f45b80fa974be0b
Adam Hellberg <sharparam@sharparam.com>
2024-12-10 23:35:58 +0100

    Update init script to use new paths

--- init.rb
+++ init.rb
@@ -21,7 +21,6 @@ begin
     opts.on('-yYEAR', '--year=YEAR', Integer, 'Year')
     opts.on('-dDAY', '--day=DAY', Integer, 'Day')
     opts.on('-sSESSION', '--session=SESSION', 'Session ID')
-    opts.on('-oFILE', '--output=FILE', 'Output filename')
   end.parse! into: options
 rescue OptionParser::MissingArgument => e
   abort e.message
@@ -33,7 +32,8 @@ dry_run = options[:'dry-run']
 year = options[:year] || now.year
 day = options[:day] || now.day
 session_id = options[:session] || AoC::Config.load('.config/aoc.toml').session
-filename = options[:output] || "src/#{year}/#{'%02d' % day}/input"
+filename = "data/aoc/#{year}/#{'%02d' % day}/input"
+linkname = "src/#{year}/#{'%02d' % day}/input"
 
 AoC::Logging.level = verbose ? AoC::Logger::TRACE : AoC::Logger::INFO
 
@@ -52,8 +52,16 @@ unless filedir == '.' || File.directory?(filedir)
   FileUtils.mkdir_p filedir
 end
 
+linkdir = File.dirname linkname
+unless File.directory?(linkdir)
+  log.debug "Creating directory for link file (#{linkdir})"
+  FileUtils.mkdir_p linkdir
+end
+
 input_content = AoC::API.input(year, day, session_id, dry_run: dry_run)
 log.info('Writing response', filename:)
 open(filename, 'w') { |f| f.write input_content } # rubocop:disable Security/Open
+FileUtils.ln_sr filename, linkname
+log.info('Created link for input file', src: filename, dst: linkname)
 
 puts 'OK' if verbose
