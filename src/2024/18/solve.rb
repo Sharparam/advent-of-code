1784500d856ea9f00e9813b2131c4ad0b03110fa
Adam Hellberg <sharparam@sharparam.com>
2024-12-18 22:45:53 +0100

    Improve Ruby solution for 2024 day 18

--- src/2024/18/solve.rb
+++ src/2024/18/solve.rb
@@ -10,22 +10,6 @@ HEIGHT = 71
 
 BYTES = ARGF.read.scan(/\d+/).map(&:to_i).each_slice(2).map { Vector[*_1] }
 
-$grids = {}
-
-def grid_at(nanosecond)
-  return $grids[nanosecond] if $grids.key?(nanosecond)
-  $grids[nanosecond] = BYTES[0..(nanosecond - 1)].to_h { [_1, :byte] }
-end
-
-def display(grid)
-  HEIGHT.times.each do |y|
-    WIDTH.times.each do |x|
-      print grid[Vector[x, y]] == :byte ? '#' : '.'
-    end
-    puts
-  end
-end
-
 DIRS = [
   Vector[1, 0], # Right
   Vector[0, 1], # Down
@@ -40,56 +24,28 @@ end
 START = Vector[0, 0]
 GOAL = Vector[WIDTH - 1, HEIGHT - 1]
 
-def dijkstra(grid)
+def dijkstra(bytes)
   dist = Hash.new Float::INFINITY
   dist[START] = 0
-  prev = {}
   heap = PairingHeap::MinPriorityQueue.new
   heap.push START, 0
 
   until heap.empty?
     u = heap.extract_min
     neighbors(u).each do |v|
-      next if grid[u] == :byte
+      next if bytes.include? v
       alt = dist[u] + 1
       next unless alt < dist[v]
-      prev[v] = u
       dist[v] = alt
       heap.push v, alt
     end
   end
 
-  [dist, prev]
+  dist
 end
 
-grid = grid_at(1024)
+puts dijkstra(BYTES[..1024].to_set)[GOAL]
 
-dist, prev = dijkstra(grid)
-
-p dist[GOAL]
-
-# p prev
-
-def make_path(prev)
-  path = Set.new
-  u = GOAL
-  return nil unless prev[u]
-  while u
-    path.add u
-    u = prev[u]
-  end
-
-  path
-end
-
-path = make_path prev
-
-BYTES[1024..].each do |byte|
-  # puts "adding byte #{byte} at ns #{nanosecond}"
-  grid[byte] = :byte
-  next unless path.include? byte
-  dist, prev = dijkstra(grid)
-  # puts "goal reached in #{d[GOAL]}"
-  break puts "#{byte[0]},#{byte[1]}" if dist[GOAL] == Float::INFINITY
-  path = make_path prev
-end
+puts BYTES[(1025...BYTES.size).bsearch { |i|
+  dijkstra(BYTES[..i].to_set)[GOAL] == Float::INFINITY
+}].to_a.join ','
