5746628b0672e1da104257a419c5b65143c1f571
Adam Hellberg <sharparam@sharparam.com>
2024-12-24 09:56:14 +0100

    Update code for 2024 day 24

--- src/2024/24/solve.rb
+++ src/2024/24/solve.rb
@@ -3,11 +3,16 @@
 
 state_str, funcs_str = ARGF.read.split("\n\n")
 
+x = []
+y = []
+
 state = state_str.lines.to_h { |line|
   k, v = line.split(': ')
-  # [k, v.to_i]
+  l, i = k[0], k[1..].to_i
+  a = l == 'x' ? x : y
   n = v.to_i
-  [k, -> { n }]
+  a[i] = n
+  [k, -> { a[i] }]
 }
 
 def mkf(s, a, b, op)
@@ -23,31 +28,52 @@ def mkf(s, a, b, op)
   end
 end
 
-funcs_str.lines.each do |func_str|
+funcs_str.lines.grep_v(/^#/).each do |func_str|
   a, op, b, c = func_str.scan(/[A-Za-z0-9]+/)
   state[c] = mkf(state, a, b, op)
 end
 
 puts state.keys.select { _1.start_with? 'z' }.sort.map { state[_1][] }.reverse.join.to_i(2)
 
-x = state.keys.select { _1.start_with? 'x' }.sort.map { state[_1][] }.reverse.join.to_i(2)
-y = state.keys.select { _1.start_with? 'y' }.sort.map { state[_1][] }.reverse.join.to_i(2)
-z = x + y
-z_digits = z.to_s(2).chars.map(&:to_i).reverse
-
-def valid(state, digits)
-  digits.each_with_index do |digit, i|
-    k = "z#{i.to_s.rjust(2, '0')}"
-    # next if state[k][] == digit
-    # raise "mismatch at #{k}: expected #{digit} but was #{state[k][]}"
-    return false unless state[k][] == digit
-  end
-
-  true
+def zs(state)
+  state.keys.select { _1.start_with? 'z' }.sort.map { state[_1][] }
 end
 
-dyn_keys = state.keys.reject { _1.start_with?('x') || _1.start_with?('y') }
+def format_bits(bits)
+  bits.each_slice(4).map { _1.reverse.join }.reverse.join ' '
+end
 
-groups = dyn_keys.permutation(8)
+def vis_add(state, x, y, pre = '')
+  warn "#{pre}   #{format_bits(x)}"
+  warn "#{pre}+  #{format_bits(y)}"
+  warn "#{pre}= #{format_bits(zs(state))}"
+end
 
-puts "#{groups.size} groups"
+x.each_with_index { |_, i| x[i] = 0 }
+y.each_with_index { |_, i| y[i] = 0 }
+
+def valid?(state, x, y, log: false)
+  valid = true
+  45.times do |i|
+    x[i] = 1
+    y[i] = 1
+    z_i = i
+    z_v = 0
+    z = zs(state)
+
+    unless z[z_i] == z_v
+      return false unless log
+      valid = false
+      warn "MISMATCH FOUND at #{i}:"
+      warn "  Expected #{x[i]} + #{y[i]} to produce #{z_v} at #{z_i} but was #{z[z_i]}"
+      vis_add(state, x, y, '    ')
+    end
+
+    x[i] = 0
+    y[i] = 0
+  end
+
+  valid
+end
+
+valid? state, x, y, log: true
