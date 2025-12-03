6a2467324a2a1a99e624b27c498dd95d885011c0
Adam Hellberg <sharparam@sharparam.com>
2025-12-03 06:44:06 +0100

    Add Ruby solution for 2025 day 3 part 2

--- src/y2025/d03/solve.rb
+++ src/y2025/d03/solve.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-banks = ARGF.map { |l| l.chomp.chars }
+banks = ARGF.map { |l| l.chomp.chars.map(&:to_i) }
 
 def max(bank, index = 0)
   max = bank[index]
@@ -15,8 +15,25 @@ def max(bank, index = 0)
   [max, index]
 end
 
-puts banks.sum { |bank|
+def max2(bank, size, current = 0)
+  return current if size.zero?
+  index = 0
+  max = bank[0]
+  bank.each_with_index do |batt, i|
+    if batt > max && (bank.size - i) >= size
+      max = batt
+      index = i
+    end
+  end
+
+  max2(bank[(index + 1)..], size - 1, (current * 10) + max)
+end
+
+puts banks.reduce([0, 0]) { |(p1, p2), bank|
   first, i = max(bank[...-1])
   second, = max(bank[(i + 1)..])
-  "#{first}#{second}".to_i
+
+  m2 = max2(bank, 12)
+
+  [p1 + "#{first}#{second}".to_i, p2 + m2]
 }
