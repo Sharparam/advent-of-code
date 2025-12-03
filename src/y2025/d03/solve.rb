d969910b6554f79cf3f6cbbb4d5a1957387ab21f
Adam Hellberg <sharparam@sharparam.com>
2025-12-03 06:45:37 +0100

    Use part 2 code for part 1

--- src/y2025/d03/solve.rb
+++ src/y2025/d03/solve.rb
@@ -3,19 +3,7 @@
 
 banks = ARGF.map { |l| l.chomp.chars.map(&:to_i) }
 
-def max(bank, index = 0)
-  max = bank[index]
-  bank[(index + 1)..].each_with_index do |batt, i|
-    if batt > max
-      max = batt
-      index = i + 1
-    end
-  end
-
-  [max, index]
-end
-
-def max2(bank, size, current = 0)
+def max(bank, size, current = 0)
   return current if size.zero?
   index = 0
   max = bank[0]
@@ -26,14 +14,12 @@ def max2(bank, size, current = 0)
     end
   end
 
-  max2(bank[(index + 1)..], size - 1, (current * 10) + max)
+  max(bank[(index + 1)..], size - 1, (current * 10) + max)
 end
 
 puts banks.reduce([0, 0]) { |(p1, p2), bank|
-  first, i = max(bank[...-1])
-  second, = max(bank[(i + 1)..])
+  m1 = max(bank, 2)
+  m2 = max(bank, 12)
 
-  m2 = max2(bank, 12)
-
-  [p1 + "#{first}#{second}".to_i, p2 + m2]
+  [p1 + m1, p2 + m2]
 }
