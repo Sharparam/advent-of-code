1a8e69d0ebbe2f2b37c7157e82d1c387e7e8c0cf
Adam Hellberg <sharparam@sharparam.com>
2025-12-07 07:41:23 +0100

    Improve Ruby solution for 2025 day 7

--- src/y2025/d07/solve.rb
+++ src/y2025/d07/solve.rb
@@ -3,51 +3,20 @@
 
 require 'matrix'
 
-require 'pairing_heap'
+splits = 0
+counters = Hash.new 0
 
-splitters = Set.new
-start = Vector[0, 0]
-height = 0
-
-ARGF.each_line(chomp: true).with_index do |line, row|
+ARGF.each_line(chomp: true) do |line|
   line.chars.each_with_index do |char, col|
     if char == ?^
-      splitters.add Vector[col, row]
+      splits += 1 if counters[col] > 0
+      counters[col - 1] += counters[col]
+      counters[col + 1] += counters[col]
+      counters[col] = 0
     elsif char == ?S
-      start = Vector[col, row]
+      counters[col] = 1
     end
   end
-  height += 1
-end
-
-beams = Set.new
-beams.add start
-
-DOWN = Vector[0, 1]
-LEFT = Vector[-1, 0]
-RIGHT = Vector[1, 0]
-
-splits = 0
-
-counters = Hash.new 0
-counters[start[0]] = 1
-
-height.times do
-  new_beams = Set.new
-  beams.each do |beam|
-    if splitters.include? beam
-      new_beams.add(beam + LEFT + DOWN)
-      new_beams.add(beam + RIGHT + DOWN)
-      splits += 1
-
-      counters[beam[0] - 1] += counters[beam[0]]
-      counters[beam[0] + 1] += counters[beam[0]]
-      counters[beam[0]] = 0
-    else
-      new_beams.add(beam + DOWN)
-    end
-  end
-  beams = new_beams
 end
 
 puts splits
