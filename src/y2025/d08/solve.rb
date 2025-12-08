70d943f945d6ecabfb17eb893612e9d190311399
Adam Hellberg <sharparam@sharparam.com>
2025-12-08 07:14:51 +0100

    Add Ruby solution for 2025 day 8 part 2

--- src/y2025/d08/solve.rb
+++ src/y2025/d08/solve.rb
@@ -3,12 +3,6 @@
 
 require 'matrix'
 
-class Vector
-  def to_s
-    "#{self[0]},#{self[1]},#{self[2]}"
-  end
-end
-
 STEPS = ENV['STEPS']&.then(&:to_i) || 1000
 
 def dist(a, b)
@@ -19,16 +13,11 @@ boxes = ARGF.map { Vector[*_1.split(',').map(&:to_i)] }
 
 distances = boxes.combination(2).map { |a, b| [a, b, dist(a, b)] }.sort_by { _1[2] }
 
-# distances.each do |a, b, d|
-#   puts "#{a} -> #{b} = #{d}"
-# end
-
 circuits = []
 
-# counter = 0
-
-STEPS.times do
-  a, b, d = distances.shift
+counter = 0
+loop do
+  a, b, = distances.shift
   existing = circuits.select { _1.include?(a) || _1.include?(b) }
   abort 'should never match more than 2' if existing.size > 2
   if existing.size == 2
@@ -37,28 +26,19 @@ STEPS.times do
   end
   existing = existing[0]
   if existing
-    # next if existing.include?(a) && existing.include?(b)
-    # puts "adding #{a} and #{b} to existing circuit"
     existing.add a
     existing.add b
   else
-    # puts "creating new circuit with #{a} and #{b}"
     circuit = Set.new
     circuit.add a
     circuit.add b
     circuits.push circuit
   end
-  # counter += 1
-  # circuits.each do |circuit|
-  #   puts "#{circuit.join(' - ')}: #{circuit.size}"
-  # end
-  # puts "\n"
+
+  counter += 1
+  puts circuits.map(&:size).sort[-3..].reduce(:*) if counter == STEPS
+
+  next unless existing && boxes.all? { existing.include? _1 }
+  puts a[0] * b[0]
+  break
 end
-
-# p circuits.map(&:size)
-
-# circuits.each do |circuit|
-#   puts "#{circuit.join(' - ')}: #{circuit.size}"
-# end
-
-puts circuits.map(&:size).sort[-3..].reduce(:*)
