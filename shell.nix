acdee2ac2bd7e9e32c938c6f5018be75780bbe7c
Adam Hellberg <sharparam@sharparam.com>
2025-12-09 13:51:38 +0100

    Add uv to nix environment

--- shell.nix
+++ shell.nix
@@ -1,7 +1,11 @@
 {pkgs ? import <nixpkgs> {}, ...}:
 pkgs.mkShell {
   buildInputs = with pkgs; [
+    # Ruby
     ruby_3_4
     openssl # Needed to build some Ruby native extensions
+
+    # Python
+    uv
   ];
 }
